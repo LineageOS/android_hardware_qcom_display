0cd152be90180c1f88ed38f1bb9663d68cb8f3f3
Amara Venkata Mastan Manoj Kumar <manojavm@codeaurora.org>
2013-07-01 13:59:34 -0700

    msm8960/8974: hwc: Refactor of allowing virtual displays to HWC
      1. Since SF doesnt invoke blank for virtual display,
         handle pipe clean up as part of Primary display
         blank.
      2. Support virtual display to query display attributes.
    
    This refactor is done to address Framework reboot issue.
    
    Change-Id: I266a87228193fd85252a22242b66dd4af5f3033f

--- msm8960/libhwcomposer/hwc_utils.h
+++ msm8960/libhwcomposer/hwc_utils.h
@@ -39,7 +39,6 @@
 #define MAX_DISPLAY_DIM 2048
 
 // For support of virtual displays
-#define MAX_DISPLAYS            (HWC_NUM_DISPLAY_TYPES)
 #define DAEMON_SOCKET "pps"
 //Fwrd decls
 struct hwc_context_t;
@@ -302,7 +301,7 @@ struct hwc_context_t {
     const hwc_procs_t* proc;
 
     //CopyBit objects
-    qhwc::CopyBit *mCopyBit[MAX_DISPLAYS];
+    qhwc::CopyBit *mCopyBit[HWC_NUM_DISPLAY_TYPES];
 
     //Overlay object - NULL for non overlay devices
     overlay::Overlay *mOverlay;
@@ -310,18 +309,18 @@ struct hwc_context_t {
     overlay::RotMgr *mRotMgr;
 
     //Primary and external FB updater
-    qhwc::IFBUpdate *mFBUpdate[MAX_DISPLAYS];
+    qhwc::IFBUpdate *mFBUpdate[HWC_NUM_DISPLAY_TYPES];
     // External display related information
     qhwc::ExternalDisplay *mExtDisplay;
     qhwc::MDPInfo mMDP;
     qhwc::VsyncState vstate;
-    qhwc::DisplayAttributes dpyAttr[MAX_DISPLAYS];
-    qhwc::ListStats listStats[MAX_DISPLAYS];
-    qhwc::LayerProp *layerProp[MAX_DISPLAYS];
-    qhwc::LayerRotMap *mLayerRotMap[MAX_DISPLAYS];
-    qhwc::MDPComp *mMDPComp[MAX_DISPLAYS];
+    qhwc::DisplayAttributes dpyAttr[HWC_NUM_DISPLAY_TYPES];
+    qhwc::ListStats listStats[HWC_NUM_DISPLAY_TYPES];
+    qhwc::LayerProp *layerProp[HWC_NUM_DISPLAY_TYPES];
+    qhwc::LayerRotMap *mLayerRotMap[HWC_NUM_DISPLAY_TYPES];
+    qhwc::MDPComp *mMDPComp[HWC_NUM_DISPLAY_TYPES];
     qhwc::CablProp mCablProp;
-    overlay::utils::Whf mPrevWHF[MAX_DISPLAYS];
+    overlay::utils::Whf mPrevWHF[HWC_NUM_DISPLAY_TYPES];
 
     //Securing in progress indicator
     bool mSecuring;
