8e7bb52bef92e0dca9d2b5c5384bf1895a67dec0
Steve Kondik <steve@cyngn.com>
2015-09-20 17:11:20 -0700

    hwc: Set ioprio for vsync thread
     * We're not doing this automatically anymore.
    
    Change-Id: I0ba52fe4648938bd7a0309bc03dc439e3d906970

--- sdm/libs/hwc2/Android.mk
+++ sdm/libs/hwc2/Android.mk
@@ -22,7 +22,7 @@ LOCAL_SHARED_LIBRARIES        := libsdmcore libqservice libbinder libhardware li
                                  libutils libcutils libsync libqdutils libqdMetaData libdl libdrmutils \
                                  libsdmutils libc++ liblog libgrallocutils libdl \
                                  vendor.display.config@1.0_vendor libhidlbase libhidltransport \
-                                 libui libgpu_tonemapper
+                                 libui libgpu_tonemapper libbfqio_vendor
 
 ifneq ($(TARGET_USES_GRALLOC1), true)
     LOCAL_SHARED_LIBRARIES += libmemalloc
