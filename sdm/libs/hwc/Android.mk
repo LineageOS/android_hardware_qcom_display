68d453ef7155606a8af21cebbd4b16aec65300a2
Steve Kondik <steve@cyngn.com>
2015-09-20 17:11:20 -0700

    hwc: Set ioprio for vsync thread
    Author: Steve Kondik <steve@cyngn.com>
    Date:   Sun Sep 20 17:11:20 2015 -0700
    
        hwc: Set ioprio for vsync thread
    
         * We're not doing this automatically anymore.
    
        Change-Id: I0ba52fe4648938bd7a0309bc03dc439e3d906970
    
    Author: dianlujitao <dianlujitao@lineageos.org>
    Date:   Fri Mar 1 11:23:27 2019 +0800
    
        hwc: Remove _vendor from libbfqio
    
        Change-Id: I99c857ed79138222a16c79dc9d4a30bdb0205e9b
    
    Change-Id: Ic7b551ef1050abd9184e620c58a3a7f8f64f1817

--- sdm/libs/hwc/Android.mk
+++ sdm/libs/hwc/Android.mk
@@ -17,7 +17,7 @@ LOCAL_CLANG                   := true
 LOCAL_SHARED_LIBRARIES        := libsdmcore libqservice libbinder libhardware libhardware_legacy \
                                  libutils libcutils libsync libmemalloc libqdutils libdl \
                                  libpowermanager libsdmutils libgpu_tonemapper  libc++ liblog \
-                                 libdrmutils libui
+                                 libdrmutils libui libbfqio
 
 LOCAL_SRC_FILES               := hwc_session.cpp \
                                  hwc_display.cpp \
