ae340ef9e56efddd33ecb418e98c0f1f34ad39f1
Arne Coucheron <arco68@gmail.com>
2016-09-11 01:39:11 +0300

    msm8084: Use generated kernel headers
    Change-Id: I161486572b58c47641499842640c69ed666e5289

--- msm8084/libvirtual/Android.mk
+++ msm8084/libvirtual/Android.mk
@@ -5,10 +5,10 @@ include $(CLEAR_VARS)
 LOCAL_MODULE                  := libvirtual
 LOCAL_MODULE_PATH             := $(TARGET_OUT_SHARED_LIBRARIES)
 LOCAL_MODULE_TAGS             := optional
-LOCAL_C_INCLUDES              := $(common_includes) $(kernel_includes)
+LOCAL_C_INCLUDES              := $(common_includes)
 LOCAL_SHARED_LIBRARIES        := $(common_libs) liboverlay libqdutils libmedia
 LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"qdvirtual\"
-LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps)
+LOCAL_HEADER_LIBRARIES        := generated_kernel_headers
 LOCAL_SRC_FILES               := virtual.cpp
 
 include $(BUILD_SHARED_LIBRARY)
