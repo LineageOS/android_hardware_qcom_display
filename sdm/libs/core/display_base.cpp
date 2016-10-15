1d59acb9db90a00158039424e8cf3dd3e755288c
Pullakavi Srinivas <spullaka@codeaurora.org>
2016-10-15 14:11:49 +0530

    SDM: Set safemode on resume.
    Display Resources are purged on suspend.
    Safemode needs to be triggered on resume in the
    presence of multiple displays.
    
    CRs-Fixed: 1078534
    Change-Id: I4b748b51bc5e2b4110aa37348a16bece566372b2

--- sdm/libs/core/display_base.cpp
+++ sdm/libs/core/display_base.cpp
@@ -1,5 +1,5 @@
 /*
-* Copyright (c) 2014 - 2016, The Linux Foundation. All rights reserved.
+* Copyright (c) 2014 - 2017, The Linux Foundation. All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without modification, are permitted
 * provided that the following conditions are met:
@@ -415,7 +415,6 @@ DisplayError DisplayBase::SetDisplayState(DisplayState state) {
     hw_layers_.info.hw_layers.clear();
     error = hw_intf_->Flush();
     if (error == kErrorNone) {
-      comp_manager_->Purge(display_comp_ctx_);
       error = hw_intf_->PowerOff();
     }
     break;
@@ -456,6 +455,7 @@ DisplayError DisplayBase::SetDisplayState(DisplayState state) {
   if (error == kErrorNone) {
     active_ = active;
     state_ = state;
+    comp_manager_->SetDisplayState(display_comp_ctx_, state, display_type_);
   }
 
   return error;
