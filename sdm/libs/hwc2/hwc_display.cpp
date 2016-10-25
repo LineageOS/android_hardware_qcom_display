06ccd3060fd29e61bebb6b9bf794b940ca41a0e3
Naseer Ahmed <naseer@codeaurora.org>
2016-10-25 12:46:33 -0400

    hwc2: Check for empty list in AcceptDisplayChanges
    The layer list can be in the flush case and is already handle in
    validate and present
    
    Change-Id: Ib29d8d79cd74e32846b66e0e72d7607735645a80
    CRs-Fixed: 1081053

--- sdm/libs/hwc2/hwc_display.cpp
+++ sdm/libs/hwc2/hwc_display.cpp
@@ -774,14 +774,22 @@ HWC2::Error HWCDisplay::PrepareLayerStack(uint32_t *out_num_types, uint32_t *out
 }
 
 HWC2::Error HWCDisplay::AcceptDisplayChanges() {
-  if (!validated_ && !layer_set_.empty()) {
+  if (layer_set_.empty()) {
+    return HWC2::Error::None;
+  }
+
+  if (!validated_) {
     return HWC2::Error::NotValidated;
   }
 
   for (const auto& change : layer_changes_) {
     auto hwc_layer = layer_map_[change.first];
     auto composition = change.second;
-    hwc_layer->UpdateClientCompositionType(composition);
+    if (hwc_layer != nullptr) {
+      hwc_layer->UpdateClientCompositionType(composition);
+    } else {
+      DLOGW("Invalid layer: %" PRIu64, change.first);
+    }
   }
   return HWC2::Error::None;
 }
