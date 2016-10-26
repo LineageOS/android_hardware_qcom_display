18630d32255a70f53e8693d23a7c7d9ec10e850b
Naseer Ahmed <naseer@codeaurora.org>
2016-10-26 16:14:19 -0400

    hwc2: Set layer buffer size
    The layer buffer size is used by the rotator
    
    CRs-Fixed: 1084674
    Change-Id: I1db5497de26b17e8d6a640a46a3b2e1296726528

--- sdm/libs/hwc2/hwc_layers.cpp
+++ sdm/libs/hwc2/hwc_layers.cpp
@@ -107,6 +107,7 @@ HWC2::Error HWCLayer::SetLayerBuffer(buffer_handle_t buffer, int32_t acquire_fen
   layer_buffer->planes[0].offset = handle->offset;
   layer_buffer->planes[0].stride = UINT32(handle->width);
   layer_buffer->acquire_fence_fd = acquire_fence;
+  layer_buffer->size = handle->size;
   layer_buffer->buffer_id = reinterpret_cast<uint64_t>(handle);
 
   return HWC2::Error::None;
