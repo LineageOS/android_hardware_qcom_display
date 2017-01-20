4cfb380e8f194d099e9dfd2305f3ffb953e3c539
Prabhanjan Kandula <pkandula@codeaurora.org>
2017-01-20 00:08:28 -0800

    sdm: Intf change propagating fb config for partial update
    Pass fb config as param to partial update interface.
    PU interface implementation can use this fb config to
    determine any scaling at layer mixer.
    
    Change-Id: I7cfe1643ff3f0b7ca36ae5aee2fd031a12fe921a
    CRs-fixed: 1108486

--- sdm/libs/core/strategy.cpp
+++ sdm/libs/core/strategy.cpp
@@ -1,5 +1,5 @@
 /*
-* Copyright (c) 2014 - 2016, The Linux Foundation. All rights reserved.
+* Copyright (c) 2014 - 2017, The Linux Foundation. All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without modification, are permitted
 * provided that the following conditions are met:
@@ -57,7 +57,7 @@ DisplayError Strategy::Init() {
     }
 
     error = extension_intf_->CreatePartialUpdate(display_type_, hw_resource_info_, hw_panel_info_,
-                                                 mixer_attributes_, display_attributes_,
+                                                 mixer_attributes_, display_attributes_, fb_config_,
                                                  &partial_update_intf_);
   }
 
@@ -217,7 +217,7 @@ DisplayError Strategy::Reconfigure(const HWPanelInfo &hw_panel_info,
   }
 
   extension_intf_->CreatePartialUpdate(display_type_, hw_resource_info_, hw_panel_info,
-                                       mixer_attributes, display_attributes,
+                                       mixer_attributes, display_attributes, fb_config,
                                        &partial_update_intf_);
 
   error = strategy_intf_->Reconfigure(hw_panel_info, hw_resource_info_, mixer_attributes,
