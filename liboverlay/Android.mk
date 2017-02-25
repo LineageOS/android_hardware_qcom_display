9b5f1e6ef023aae22786e103421bad5f3ea4554b
Diego Wilson <diegowilson@google.com>
2017-02-24 17:07:21 -0800

    Move some qcom libraries to vendor partition
    SoC-specific libraries must go to vendor partition so that we can build
    a generic system image that runs on multiple devices.
    
    For now, graphics libraries for msm8996 are moved. Ideally, almost
    everything under /hardware/qcom will be moved to vendor.
    
    Bug: 34826389
    Bug: 35232083
    Test: build & run any msm8996 based device (such as sailfish/marlin)
    Change-Id: Ic089584186e4c74dfdc2c3af4bc7d2777b084828

--- liboverlay/Android.mk
+++ liboverlay/Android.mk
@@ -3,8 +3,8 @@ include $(LOCAL_PATH)/../common.mk
 include $(CLEAR_VARS)
 
 LOCAL_MODULE                  := liboverlay
-LOCAL_MODULE_PATH             := $(TARGET_OUT_SHARED_LIBRARIES)
 LOCAL_MODULE_TAGS             := optional
+LOCAL_PROPRIETARY_MODULE      := true
 LOCAL_C_INCLUDES              := $(common_includes) $(kernel_includes)
 LOCAL_SHARED_LIBRARIES        := $(common_libs) libqdutils libmemalloc \
                                  libsync libdl
