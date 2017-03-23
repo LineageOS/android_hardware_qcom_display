3638ebaee3775033124909707611410d837ac9eb
Naseer Ahmed <naseer@codeaurora.org>
2017-03-23 12:14:01 -0400

    hwc2: Do not delete hwc_session
    HWC session is derived from hwc2_device and is deleted by
    libhardware.
    
    CRs-Fixed: 2023877
    Bug: 34206930
    Change-Id: Ic983a232f0ca417c0b47d5ea3c59e7374d7e7fea

--- msm8996/sdm/libs/hwc2/hwc_session.cpp
+++ msm8996/sdm/libs/hwc2/hwc_session.cpp
@@ -177,7 +177,6 @@ int HWCSession::Open(const hw_module_t *module, const char *name, hw_device_t **
 
     int status = hwc_session->Init();
     if (status != 0) {
-      delete hwc_session;
       return status;
     }
 
@@ -199,7 +198,6 @@ int HWCSession::Close(hw_device_t *device) {
   HWCSession *hwc_session = static_cast<HWCSession *>(composer_device);
 
   hwc_session->Deinit();
-  delete hwc_session;
 
   return 0;
 }
