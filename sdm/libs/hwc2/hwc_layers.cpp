14c8e5ba8a6cd161c7dfaccd899676c9ff1d6a84
Saurabh Shah <saurshah@codeaurora.org>
2017-04-07 10:37:23 -0700

    hwc2/gralloc1: Create libgrallocutils
    --For gralloc1, create a libgrallocutils that provides helpers to
    calculate various buffer related info
    --Make hwc2/gralloc1 structures independent of libdrmutils
    and fb_id / gemhandles
    --Implement GetBufferLayout API from BufferAllocator that can be called
    by DAL before creating fb_id
    
    Change-Id: I0aede7d1ea87302ecda593047728d381383484b9
    CRs-fixed: 1114808

--- sdm/libs/hwc2/hwc_layers.cpp
+++ sdm/libs/hwc2/hwc_layers.cpp
@@ -154,7 +154,6 @@ HWC2::Error HWCLayer::SetLayerBuffer(buffer_handle_t buffer, int32_t acquire_fen
   layer_buffer->acquire_fence_fd = acquire_fence;
   layer_buffer->size = handle->size;
   layer_buffer->buffer_id = reinterpret_cast<uint64_t>(handle);
-  layer_buffer->fb_id = handle->fb_id;
 
   return HWC2::Error::None;
 }
