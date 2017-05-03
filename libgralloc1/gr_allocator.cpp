b2033f1be8995df17c7a2857b164b45eae75a7f9
Naseer Ahmed <naseer@codeaurora.org>
2017-05-02 21:10:29 -0400

    gralloc1: Allocate sensor data from ADSP heap
    CRs-Fixed: 2041813
    Change-Id: I15489e9cb1f20c650fba31c827a22f5ae306b842

--- libgralloc1/gr_allocator.cpp
+++ libgralloc1/gr_allocator.cpp
@@ -289,7 +289,8 @@ void Allocator::GetIonHeapInfo(gralloc1_producer_usage_t prod_usage,
     heap_id |= ION_HEAP(ION_CAMERA_HEAP_ID);
   }
 
-  if (prod_usage & GRALLOC1_PRODUCER_USAGE_PRIVATE_ADSP_HEAP) {
+  if (prod_usage & GRALLOC1_PRODUCER_USAGE_PRIVATE_ADSP_HEAP ||
+      prod_usage & GRALLOC1_PRODUCER_USAGE_SENSOR_DIRECT_DATA) {
     heap_id |= ION_HEAP(ION_ADSP_HEAP_ID);
   }
 
