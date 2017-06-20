ddc832407aade2436e1f151a392c0565c08ce63e
Yifan Hong <elsk@google.com>
2017-06-20 14:11:31 -0700

    Mark msm8998 modules LOCAL_VENDOR_MODULE
    Test: m -j -k BOARD_VNDK_VERSION=true
    
    Bug: 37342627
    
    Change-Id: Ia32500a8ab329a4cf30b64ae5a7738b1aecec80d

--- gpu_tonemapper/Android.mk
+++ gpu_tonemapper/Android.mk
@@ -2,6 +2,7 @@ LOCAL_PATH := $(call my-dir)
 include $(LOCAL_PATH)/../common.mk
 
 include $(CLEAR_VARS)
+LOCAL_VENDOR_MODULE       := true
 LOCAL_COPY_HEADERS_TO     := $(common_header_export_path)
 LOCAL_COPY_HEADERS        := TonemapFactory.h Tonemapper.h
 include $(BUILD_COPY_HEADERS)
