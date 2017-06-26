d352ce80b219285c3f676d893c6634052636021e
Jiyong Park <jiyong@google.com>
2017-06-26 13:45:11 +0900

    Use header lib instead of using global include path
    Header lib is preferred since a set of headers can have internal
    dependency to other headers which are in different directories.
    
    Bug: 37342627
    Test: BOARD_VNDK_VERSION=current m -j memtrack.msm8996
    Change-Id: I19b05ea960ba553eb9bbcd674eb9c50216169cef

--- libmemtrack/Android.mk
+++ libmemtrack/Android.mk
@@ -20,9 +20,9 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_VENDOR_MODULE := true
-LOCAL_C_INCLUDES += hardware/libhardware/include
 LOCAL_CFLAGS := -Wconversion -Wall -Werror -Wno-sign-conversion
 LOCAL_SHARED_LIBRARIES := liblog
 LOCAL_SRC_FILES := memtrack_msm.c kgsl.c
 LOCAL_MODULE := memtrack.$(TARGET_BOARD_PLATFORM)
+LOCAL_HEADER_LIBRARIES := libhardware_headers
 include $(BUILD_SHARED_LIBRARY)
