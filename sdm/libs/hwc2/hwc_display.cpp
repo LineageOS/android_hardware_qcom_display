bc846952d1800e9ddbc26895d7b7132c0ff1366c
Pramodh Kumar Mukunda <pkmuku@codeaurora.org>
2017-06-29 18:28:20 +0530

    sdm: Change frame dump directory
    -Change frame dump directory to "/data/vendor/display".
    
    Change-Id: I85c32b6768ab0669c6910d5e3fce64d1aa3aeb5c
    CRs-Fixed: 2065996
    Signed-off-by: Pramodh Kumar Mukunda <pkmuku@codeaurora.org>

--- sdm/libs/hwc2/hwc_display.cpp
+++ sdm/libs/hwc2/hwc_display.cpp
@@ -1336,7 +1336,8 @@ void HWCDisplay::DumpInputBuffers() {
     return;
   }
 
-  snprintf(dir_path, sizeof(dir_path), "/data/misc/display/frame_dump_%s", GetDisplayString());
+  snprintf(dir_path, sizeof(dir_path), "%s/frame_dump_%s", HWCDebugHandler::DumpDir(),
+           GetDisplayString());
 
   if (mkdir(dir_path, 0777) != 0 && errno != EEXIST) {
     DLOGW("Failed to create %s directory errno = %d, desc = %s", dir_path, errno, strerror(errno));
@@ -1385,7 +1386,8 @@ void HWCDisplay::DumpInputBuffers() {
 void HWCDisplay::DumpOutputBuffer(const BufferInfo &buffer_info, void *base, int fence) {
   char dir_path[PATH_MAX];
 
-  snprintf(dir_path, sizeof(dir_path), "/data/misc/display/frame_dump_%s", GetDisplayString());
+  snprintf(dir_path, sizeof(dir_path), "%s/frame_dump_%s", HWCDebugHandler::DumpDir(),
+           GetDisplayString());
 
   if (mkdir(dir_path, 777) != 0 && errno != EEXIST) {
     DLOGW("Failed to create %s directory errno = %d, desc = %s", dir_path, errno, strerror(errno));
