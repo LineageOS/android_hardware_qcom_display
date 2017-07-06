05a342ab83d6883260bbdcddfb78d0c2f8a71c5d
Naseer Ahmed <naseer@codeaurora.org>
2017-07-06 19:37:16 -0400

    display: Mark header modules as vendor
    Change-Id: Iea70b19b2baf9827b3551b7ba16808fe89c48cf4

--- include/Android.mk
+++ include/Android.mk
@@ -2,6 +2,7 @@ LOCAL_PATH:= $(call my-dir)
 include $(LOCAL_PATH)/../common.mk
 include $(CLEAR_VARS)
 
+LOCAL_VENDOR_MODULE           := true
 LOCAL_COPY_HEADERS_TO         := $(common_header_export_path)
 LOCAL_COPY_HEADERS            := color_metadata.h
 
@@ -9,7 +10,8 @@ include $(BUILD_COPY_HEADERS)
 
 include $(CLEAR_VARS)
 #TODO move all exported headers to this directory
-LOCAL_MODULE := display_headers
+LOCAL_MODULE                  := display_headers
+LOCAL_VENDOR_MODULE           := true
 LOCAL_EXPORT_C_INCLUDE_DIRS   := $(LOCAL_PATH) \
                                  $(display_top)/libcopybit \
                                  $(display_top)/libdrmutils \
