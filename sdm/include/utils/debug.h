0a481100860f680a420df7b4f39db9f553d862ef
Anjaneya Prasad Musunuri <aprasad@codeaurora.org>
2017-07-08 11:17:55 +0530

    sdm: Add support to simulate reduced configuration
    Add support to simulate reduce plane configuration.
    
    Change-Id: Ic4633668ff255840af3a175a66d553ee017bb27b
    CRs-Fixed: 2071452

--- sdm/include/utils/debug.h
+++ sdm/include/utils/debug.h
@@ -79,6 +79,7 @@ class Debug {
   static bool IsExtAnimDisabled();
   static bool IsPartialSplitDisabled();
   static DisplayError GetMixerResolution(uint32_t *width, uint32_t *height);
+  static DisplayError GetReducedConfig(uint32_t *num_vig_pipes, uint32_t *num_dma_pipes);
   static int GetExtMaxlayers();
   static bool GetProperty(const char *property_name, char *value);
   static bool SetProperty(const char *property_name, const char *value);
