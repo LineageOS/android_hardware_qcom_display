c78e081a22760c282cc352595744d39bd3abad54
Rashed Abdel-Tawab <rashed@linux.com>
2017-07-30 02:40:38 -0400

    tonemapper: Depend on kernel headers
    Change-Id: I9874c6946b29c0dc133099f52f7a7e2efacad2e1

--- gpu_tonemapper/Android.mk
+++ gpu_tonemapper/Android.mk
@@ -11,6 +11,7 @@ LOCAL_MODULE              := libgpu_tonemapper
 LOCAL_MODULE_TAGS         := optional
 LOCAL_C_INCLUDES          := $(TARGET_OUT_HEADERS)/qcom/display/
 LOCAL_C_INCLUDES          += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
+LOCAL_ADDITIONAL_DEPENDENCIES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 
 LOCAL_CFLAGS              := $(version_flag) -Wno-missing-field-initializers -Wall \
                              -Wno-unused-parameter -std=c++11 -DLOG_TAG=\"GPU_TONEMAPPER\"
