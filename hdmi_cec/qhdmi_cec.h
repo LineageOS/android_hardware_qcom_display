563e44a7aa6ae887c7519ac10947cbaaad0f2fd6
Vijetha Bhat <bhat@codeaurora.org>
2017-08-02 15:18:27 +0530

    Hdmi cec: Monitor hotplug & cec messages in a single thread
    Restructuring hdmi cec hal to monitor hdmi hotplug events and
    cec message events in a single thread as against two threads.
    
    Change-Id: I9b0d7b7ea8f6401f45d09f3f451b6336edecc5cb
    CRs-Fixed: 2086288

--- hdmi_cec/qhdmi_cec.h
+++ hdmi_cec/qhdmi_cec.h
@@ -1,5 +1,5 @@
 /*
-* Copyright (c) 2014 The Linux Foundation. All rights reserved.
+* Copyright (c) 2017 The Linux Foundation. All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without
 * modification, are permitted provided that the following conditions are
@@ -30,16 +30,17 @@
 #define QHDMI_CEC_H
 
 #include <hardware/hdmi_cec.h>
-#include <utils/RefBase.h>
-
-namespace qClient {
-    class QHDMIClient;
-};
+#include <sys/poll.h>
+#include <sys/prctl.h>
+#include <sys/socket.h>
+#include <sys/resource.h>
+#include <linux/netlink.h>
+#include <thread>
+#include <vector>
 
 namespace qhdmicec {
 
-#define SYSFS_BASE  "/sys/class/graphics/fb"
-#define MAX_PATH_LENGTH  128
+static const int MAX_STRING_LENGTH = 1024;
 
 struct cec_callback_t {
     // Function in HDMI service to call back on CEC messages
@@ -49,6 +50,8 @@ struct cec_callback_t {
 
 };
 
+struct eventData;
+
 struct cec_context_t {
     hdmi_cec_device_t device;    // Device for HW module
     cec_callback_t callback;     // Struct storing callback object
@@ -56,7 +59,7 @@ struct cec_context_t {
     bool arc_enabled;
     bool system_control;         // If true, HAL/driver handle CEC messages
     int fb_num;                  // Framebuffer node for HDMI
-    char fb_sysfs_path[MAX_PATH_LENGTH];
+    std::string fb_sysfs_path;
     hdmi_port_info *port_info;   // HDMI port info
 
     // Logical address is stored in an array, the index of the array is the
@@ -64,7 +67,21 @@ struct cec_context_t {
     int logical_address[CEC_ADDR_BROADCAST];
     int version;
     uint32_t vendor_id;
-    android::sp<qClient::QHDMIClient> disp_client;
+
+    std::vector<pollfd> poll_fds;               // poll fds for cec message monitor and exit signal
+                                                // on cec message monitor thread
+    int exit_fd = -1;
+    bool cec_exit_thread = false;
+    std::thread hdmi_cec_monitor;               // hdmi plugin monitor thread variable
+    char data[MAX_STRING_LENGTH] = {0};
+
+    std::vector<std::string> node_list = {};
+    std::vector<eventData> event_data_list = {};
+};
+
+struct eventData {
+    const char* event_name = NULL;
+    void (*event_parser)(cec_context_t* ctx, uint32_t node_event) = NULL;
 };
 
 void cec_receive_message(cec_context_t *ctx, char *msg, ssize_t len);
