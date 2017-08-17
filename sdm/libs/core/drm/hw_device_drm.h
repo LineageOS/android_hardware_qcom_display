5dcd59c4bd304e021075d7d33c7955a37e00ac3b
Ramkumar Radhakrishnan <ramkumar@codeaurora.org>
2017-08-17 14:19:15 +0530

    sdm: Add DRC support for WFD
    1. Configure WB based on output buffer resolution.
    2. Create new WB mode based on output buffer resolution and add it to
       the mode list.
    3. Inform WB driver about the list of modes to be supported using
       DRM_IOCTL_SDE_WB_CONFIG ioctl.
    
    Change-Id: Id25c414c252a6d7f4ac95141db3d0685db7262c6
    CRs-Fixed: 2103566

--- sdm/libs/core/drm/hw_device_drm.h
+++ sdm/libs/core/drm/hw_device_drm.h
@@ -110,7 +110,6 @@ class HWDeviceDRM : public HWInterface {
   void PopulateHWPanelInfo();
   void GetHWDisplayPortAndMode();
   void GetHWPanelMaxBrightness();
-  void ResetDisplayParams();
   bool EnableHotPlugDetection(int enable);
   void UpdateMixerAttributes();
   void InitializeConfigs();
