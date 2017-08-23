9c3de95b2b4b8f6b220dc42da3116d949fd82990
Dan Pasanen <dan.pasanen@gmail.com>
2017-08-23 00:02:34 -0500

    libhwcomposer: add libarect static lib
    Change-Id: I94a240a0b64e432c380b8564f6e910cb500ad3b9

--- libhwcomposer/Android.mk
+++ libhwcomposer/Android.mk
@@ -12,6 +12,7 @@ LOCAL_SHARED_LIBRARIES        := $(common_libs) libEGL liboverlay \
                                  libhdmi libqdutils libhardware_legacy \
                                  libdl libmemalloc libqservice libsync \
                                  libbinder libmedia libskia libvirtual
+LOCAL_STATIC_LIBRARIES        := libarect
 LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"qdhwcomposer\"
 #Enable Dynamic FPS if PHASE_OFFSET is not set
 ifeq ($(VSYNC_EVENT_PHASE_OFFSET_NS),)
