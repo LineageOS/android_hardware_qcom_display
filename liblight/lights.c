edd9998668309a11b5e605f9e22e05827b4cfeb1
Arun Kumar K.R <akumarkr@codeaurora.org>
2017-08-23 08:09:06 +0530

    Revert "liblights: Remove unsupported button light node."
    This reverts commit 42a514ab089ee2985cbb9fba0fb5fb8819989c72.
    
    Change-Id: Ia06ea393ffc737479b9199216b3e436d3ea10a28
    Crs-fixed: 2093165

--- liblight/lights.c
+++ liblight/lights.c
@@ -307,6 +307,20 @@ set_light_attention(struct light_device_t* dev,
     return 0;
 }
 
+static int
+set_light_buttons(struct light_device_t* dev,
+        struct light_state_t const* state)
+{
+    int err = 0;
+    if(!dev) {
+        return -1;
+    }
+    pthread_mutex_lock(&g_lock);
+    err = write_int(BUTTON_FILE, state->color & 0xFF);
+    pthread_mutex_unlock(&g_lock);
+    return err;
+}
+
 /** Close the lights device */
 static int
 close_lights(struct light_device_t *dev)
@@ -347,6 +361,8 @@ static int open_lights(const struct hw_module_t* module, char const* name,
         set_light = set_light_battery;
     else if (0 == strcmp(LIGHT_ID_NOTIFICATIONS, name))
         set_light = set_light_notifications;
+    else if (0 == strcmp(LIGHT_ID_BUTTONS, name))
+        set_light = set_light_buttons;
     else if (0 == strcmp(LIGHT_ID_ATTENTION, name))
         set_light = set_light_attention;
     else
