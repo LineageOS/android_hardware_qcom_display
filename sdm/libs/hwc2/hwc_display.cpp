e88b3f4a513afe486f8a4756bf407a0fa5184ef6
Saurabh Dubey <sdubey@codeaurora.org>
2017-10-10 14:21:22 +0530

    hwc2: Array and enum boundary checks
    Change-Id: If7c888656cddd5b7f62e138b50e6f2bb69550376
    Crs-fixed: 2114346

--- sdm/libs/hwc2/hwc_display.cpp
+++ sdm/libs/hwc2/hwc_display.cpp
@@ -136,11 +136,7 @@ HWC2::Error HWCColorMode::RestoreColorTransform() {
 }
 
 HWC2::Error HWCColorMode::SetColorTransform(const float *matrix, android_color_transform_t hint) {
-  if (!matrix || (hint < HAL_COLOR_TRANSFORM_IDENTITY ||
-      hint > HAL_COLOR_TRANSFORM_CORRECT_TRITANOPIA)) {
-    return HWC2::Error::BadParameter;
-  }
-
+  DTRACE_SCOPED();
   double color_matrix[kColorTransformMatrixCount] = {0};
   CopyColorTransformMatrix(matrix, color_matrix);
 
