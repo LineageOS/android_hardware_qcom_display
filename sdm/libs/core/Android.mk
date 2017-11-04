644a0910ef2068d322c5e22b03d89c83f5ef1a4b
Ian Roy <ian.t.roy@gmail.com>
2017-11-04 10:01:22 -0500

    sdm: add board flag for panel shift
    Change-Id: I0939619506b82d75a5db985dabc9297142851efe

--- sdm/libs/core/Android.mk
+++ sdm/libs/core/Android.mk
@@ -22,6 +22,14 @@ ifeq ($(TARGET_USES_DRM_PP),true)
     LOCAL_CFLAGS              += -DPP_DRM_ENABLE
 endif
 
+ifneq ($(TARGET_DISPLAY_SHIFT_HORIZONTAL),)
+    LOCAL_CFLAGS += -DDISPLAY_SHIFT_HORIZONTAL=$(TARGET_DISPLAY_SHIFT_HORIZONTAL)
+endif
+
+ifneq ($(TARGET_DISPLAY_SHIFT_VERTICAL),)
+    LOCAL_CFLAGS += -DDISPLAY_SHIFT_VERTICAL=$(TARGET_DISPLAY_SHIFT_VERTICAL)
+endif
+
 LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps) $(kernel_deps)
 LOCAL_SRC_FILES               := core_interface.cpp \
                                  core_impl.cpp \
