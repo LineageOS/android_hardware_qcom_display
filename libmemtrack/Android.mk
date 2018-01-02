8acc560292d843c61424ee33e32b878859a0072b
Chih-Hung Hsieh <chh@google.com>
2018-01-02 11:34:10 -0800

    Add -Wno-error to compile with global -Werror.
    Bug: 66996870
    Test: normal builds.
    Change-Id: Iee4ac5f95a2013c5eeac1c061889a593caf0ec36

--- libmemtrack/Android.mk
+++ libmemtrack/Android.mk
@@ -25,4 +25,5 @@ LOCAL_CFLAGS := -Wconversion -Wall -Werror -Wno-sign-conversion
 LOCAL_SHARED_LIBRARIES := liblog
 LOCAL_SRC_FILES := memtrack_msm.c kgsl.c
 LOCAL_MODULE := memtrack.$(TARGET_BOARD_PLATFORM)
+LOCAL_CFLAGS += -Wno-error
 include $(BUILD_SHARED_LIBRARY)
