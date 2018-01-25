31174f4806cf22de682628207a91857c5b2e39d8
Steven Moreland <smoreland@google.com>
2018-01-25 12:29:25 -0800

    Import headers for enabling VNDK.
    These modules don't explicitly declare their
    dependence on hardware/gralloc1.h
    
    Bug: 69526027
    Test: mma -j BOARD_VNDK_VERSION=current
    Change-Id: If9e5636635c7d039d7fa924cb51fa5f838ee6bca

--- msm8996/libgralloc1/Android.mk
+++ msm8996/libgralloc1/Android.mk
@@ -15,6 +15,7 @@ LOCAL_MODULE_TAGS             := optional
 LOCAL_C_INCLUDES              := $(common_includes) \
                                  external/libcxx/include/
 
+LOCAL_HEADER_LIBRARIES        := libhardware_headers
 LOCAL_SHARED_LIBRARIES        := $(common_libs) libqdMetaData libsync
 LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"qdgralloc\" -fPIC -Wall -std=c++11 -Werror
 LOCAL_CFLAGS                  += -isystem  $(kernel_includes)
