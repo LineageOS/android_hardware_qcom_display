9b59b3a234b495e612fc8f7cfb80c49259c54e6f
Bruno Martins <bgcngm@gmail.com>
2018-01-29 12:38:00 +0000

    [DNM][SQUASH]Merge tag 'LA.UM.6.6.r1-05700-89xx.0' into HEAD
    "LA.UM.6.6.r1-05700-89xx.0"
    
    Change-Id: I6766485d16c63ce014989cdd7cd5553bd1379fbb

--- sdm/libs/hwc2/hwc_session.cpp
+++ sdm/libs/hwc2/hwc_session.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014-2017, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2014-2018, The Linux Foundation. All rights reserved.
  * Not a Contribution.
  *
  * Copyright 2015 The Android Open Source Project
@@ -502,7 +502,9 @@ static int32_t GetHdrCapabilities(hwc2_device_t* device, hwc2_display_t display,
 }
 
 static uint32_t GetMaxVirtualDisplayCount(hwc2_device_t *device) {
-  return 1;
+  char property[PROPERTY_VALUE_MAX];
+  property_get("debug.sdm.support_writeback", property, "1");
+  return (uint32_t) atoi(property);
 }
 
 static int32_t GetReleaseFences(hwc2_device_t *device, hwc2_display_t display,
