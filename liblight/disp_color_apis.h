1650a1433d9eeacfa1641352a8521f6dc275207d
Sean hoyt <seanhoyt963@gmail.com>
2018-02-14 21:25:32 +0000

    Add reverse engineered disp_color_apis.h
    Change-Id: Ifa40ed6968abf86bcdcbdf28da66f3d675c77274

---
+++ liblight/disp_color_apis.h
@@ -0,0 +1,23 @@
+#ifndef __DISP_COLOR_APIS_H__
+#define __DISP_COLOR_APIS_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdlib.h>
+#include <sys/types.h>
+#include <cutils/log.h>
+
+typedef uint64_t DISPAPI_HANDLE;
+
+int32_t disp_api_init(DISPAPI_HANDLE *hctx, uint32_t flags);
+
+int32_t disp_api_set_panel_brightness_level_ext(DISPAPI_HANDLE hctx, uint32_t disp_id, int32_t level,
+                                          uint32_t flags);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
