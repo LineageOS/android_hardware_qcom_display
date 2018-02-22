1eaa9893342f82534b02688ad75e4eae96a0ad95
Bharath Gopal <gopalb@codeaurora.org>
2018-02-22 17:38:01 +0530

    hwc2: Avoid resetting validate flag during present when display is paused
     The Present function resets the validate_ flag when the display is in the
     paused state, which can cause subsequent calls to Present to fail for
     other displays which have executed the Validate function and are yet to
     commit their layer stack, consequentially stopping to update the diplay.
     Resetting of the flag is already taken care of when the display state is
     changed. So remove reset from the Present function.
    
    Change-Id: I62e9b79dd7222b58ea8aa3f5222d04c40cd9ae6a

--- sdm/libs/hwc2/hwc_display_primary.cpp
+++ sdm/libs/hwc2/hwc_display_primary.cpp
@@ -233,7 +233,6 @@ HWC2::Error HWCDisplayPrimary::Present(int32_t *out_retire_fence) {
     // If we do not handle the frame set retireFenceFd to outbufAcquireFenceFd
     // Revisit this when validating display_paused
     DisplayError error = display_intf_->Flush();
-    validated_.reset();
     if (error != kErrorNone) {
       DLOGE("Flush failed. Error = %d", error);
     }
