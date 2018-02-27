5b428dcb384c6baf80ed9474c4d5e865267edef2
Namit Solanki <nsolanki@codeaurora.org>
2018-02-27 11:39:05 +0530

    sdm: Drop idle timeout event if refresh is in progress.
    - Drop idle timeout event if it occurs between prepare()
      and commit() i.e. device is not really in idle state
      since a new refresh cycle has already started.
    
    Change-Id: If66353ac38bd3a5463fb7fa002e61fb8ed7c80d8
    CRs-Fixed: 2181941

--- sdm/libs/hwc2/hwc_display_builtin.cpp
+++ sdm/libs/hwc2/hwc_display_builtin.cpp
@@ -230,6 +230,7 @@ HWC2::Error HWCDisplayBuiltIn::Validate(uint32_t *out_num_types, uint32_t *out_n
   }
 
   status = PrepareLayerStack(out_num_types, out_num_requests);
+  pending_commit_ = true;
   return status;
 }
 
@@ -251,6 +252,7 @@ HWC2::Error HWCDisplayBuiltIn::Present(int32_t *out_retire_fence) {
   }
 
   CloseFd(&output_buffer_.acquire_fence_fd);
+  pending_commit_ = false;
   return status;
 }
 
