3d395f6741d7a7ce0d68c57718dc3be8286adaf5
Alexey Polyudov <apolyudov@google.com>
2018-02-27 14:05:59 -0800

    Use libhwui.so instead of libskia.so
    Change-Id: Ie60ee11910f7e0b2da20e8634c38265e86b5742c
    Signed-off-by: Alexey Polyudov <apolyudov@google.com>

--- libhwcomposer/Android.mk
+++ libhwcomposer/Android.mk
@@ -12,7 +12,7 @@ LOCAL_C_INCLUDES              := $(common_includes) $(kernel_includes) \
 LOCAL_SHARED_LIBRARIES        := $(common_libs) libEGL liboverlay \
                                  libhdmi libqdutils libhardware_legacy \
                                  libdl libmemalloc libqservice libsync \
-                                 libbinder libmedia libskia libvirtual \
+                                 libbinder libmedia libhwui libvirtual \
                                  libbfqio_vendor
 LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"qdhwcomposer\"
 #Enable Dynamic FPS if PHASE_OFFSET is not set
