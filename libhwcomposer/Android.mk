b25196fff0d9a0dbb8efe034a9fee43ad6a63997
Alexey Polyudov <apolyudov@google.com>
2018-02-27 14:05:59 -0800

    Use libhwui.so instead of libskia.so
    Change-Id: Ie60ee11910f7e0b2da20e8634c38265e86b5742c
    Signed-off-by: Alexey Polyudov <apolyudov@google.com>

--- libhwcomposer/Android.mk
+++ libhwcomposer/Android.mk
@@ -15,7 +15,7 @@ LOCAL_SHARED_LIBRARIES        := $(common_libs) libEGL liboverlay \
                                  libbinder libmedia libbfqio_vendor
 
 ifeq ($(TARGET_USES_QCOM_BSP),true)
-LOCAL_SHARED_LIBRARIES += libskia
+LOCAL_SHARED_LIBRARIES += libhwui
 endif #TARGET_USES_QCOM_BSP
 
 LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"qdhwcomposer\" -Wno-absolute-value \
