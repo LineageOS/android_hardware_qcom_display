7756bff15fad7dc2825f85be75e53a7b15f6d960
Naseer Ahmed <naseer@codeaurora.org>
2018-03-07 21:11:53 -0500

    display: Add display specific make include
    Initial display config file for sdm845
    
    Change-Id: Iaa56ba850ea86db373944ee96469e7cbe0ef8d70

---
+++ config/sdm845.mk
@@ -0,0 +1,49 @@
+#Display related packages and configuration
+
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.composer@2.1-service \
+    android.hardware.graphics.mapper@2.0-impl-qti-display \
+    vendor.qti.hardware.display.allocator@1.0-service \
+    android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service \
+    android.hardware.light@2.0-impl \
+    android.hardware.light@2.0-service \
+    modetest \
+    gralloc.sdm845 \
+    lights.sdm845 \
+    hwcomposer.sdm845 \
+    memtrack.sdm845 \
+    libqdutils \
+    libqdMetaData \
+    libqdMetaData.system
+
+
+TARGET_FORCE_HWC_FOR_VIRTUAL_DISPLAYS := true
+MAX_VIRTUAL_DISPLAY_DIMENSION := 4096
+NUM_FRAMEBUFFER_SURFACE_BUFFERS := 2
+#Enable Charging Icon
+TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
+TARGET_USES_GRALLOC1 := true
+TARGET_USES_HWC2 := true
+TARGET_USES_QCOM_DISPLAY_BSP := true
+TARGET_USES_COLOR_METADATA := true
+TARGET_USES_DRM_PP := true
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.mdpcomp.logs=0 \
+    debug.sf.enable_hwc_vds=0 \
+    persist.demo.hdmirotationlock=false \
+    ro.qualcomm.cabl=1 \
+    debug.gralloc.gfx_ubwc_disable=0 \
+    sdm.debug.disable_scalar=0 \
+    sdm.debug.disable_inline_rotator=1 \
+    sdm.debug.prefersplit=1 \
+    sdm.debug.disable_dest_scalar=0 \
+    debug.sf.hw=0 \
+    debug.egl.hw=0 \
+    debug.sf.latch_unsignaled=1
+
+
+
+
