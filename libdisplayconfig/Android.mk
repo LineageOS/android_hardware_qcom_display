de9863c5ae63ef1c9047f3b235524d7445ea8df7
Pullakavi Srinivas <spullaka@codeaurora.org>
2018-03-12 13:45:13 +0530

    displayconfig: Export header's path.
    Change-Id: Ieb8e18c68e1c1bb51698dadd81cc0a8f02721335

--- libdisplayconfig/Android.mk
+++ libdisplayconfig/Android.mk
@@ -7,6 +7,7 @@ LOCAL_HEADER_LIBRARIES        := display_headers
 LOCAL_SRC_FILES               := DisplayConfig.cpp
 LOCAL_SHARED_LIBRARIES        := libhidlbase libhidltransport libutils \
                                  vendor.display.config@1.0
+LOCAL_EXPORT_C_INCLUDE_DIRS   := $(LOCAL_PATH)
 
 ifeq ($(LLVM_SA), true)
     LOCAL_CFLAGS += --compile-and-analyze --analyzer-perf --analyzer-Werror
