35c10a8a1d106d126e273b6f4f8f83b1e050c0ad
Naseer Ahmed <naseer@codeaurora.org>
2018-03-12 20:15:07 -0400

    gralloc: Refactor size calculation
    Make GetSize return the final aligned, layer counted size.
    This can be used at other locations
    
    Change-Id: I5a8940b565eec6c8e709163c7736d485688ddb2e

--- gralloc/gr_utils.h
+++ gralloc/gr_utils.h
@@ -41,6 +41,12 @@
 #define SIZE_4K 4096
 #define SIZE_8K 4096
 
+#ifdef MASTER_SIDE_CP
+#define SECURE_ALIGN SZ_4K
+#else
+#define SECURE_ALIGN SZ_1M
+#endif
+
 #define INT(exp) static_cast<int>(exp)
 #define UINT(exp) static_cast<unsigned int>(exp)
 
@@ -49,10 +55,11 @@ using android::hardware::graphics::common::V1_1::BufferUsage;
 namespace gralloc {
 struct BufferInfo {
   BufferInfo(int w, int h, int f, uint64_t usage = 0)
-      : width(w), height(h), format(f), usage(usage) {}
+      : width(w), height(h), format(f), layer_count(1), usage(usage) {}
   int width;
   int height;
   int format;
+  int layer_count;
   uint64_t usage;
 };
 
@@ -94,6 +101,7 @@ unsigned int GetUBwcSize(int width, int height, int format, unsigned int aligned
                          unsigned int alignedh);
 int GetBufferLayout(private_handle_t *hnd, uint32_t stride[4], uint32_t offset[4],
                     uint32_t *num_planes);
+uint32_t GetDataAlignment(int format, uint64_t usage);
 
 void GetGpuResourceSizeAndDimensions(const BufferInfo &info, unsigned int *size,
                                      unsigned int *alignedw, unsigned int *alignedh,
