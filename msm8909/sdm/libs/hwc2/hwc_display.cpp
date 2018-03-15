dc92f5745c2e2ab12e56d4ff5ccb7b7e2773438f
Saurabh Shah <saurshah@codeaurora.org>
2018-03-15 16:41:48 +0530

    hwc: Fix request flag reset and Present() bailout
    The ClearRequestFlags() method iterates over layer_stack_ instead of
    layer_set_. This causes a use-after-free since DestroyLayer() removes
    layer from layer_set_ only. Besides that, the method need not be
    virtual and has a redundant loop. This change removes the
    ClearRequestFlags() method and clears flags in an existing loop.
    Present() bails out if layer_set_ is empty or a shutdown is pending.
    Since layers could have been destroyed before Present(), ideally
    bailing out on re-validation has a higher priority over the rest
    
    CRs-fixed: 2165926
    Change-Id: Ib1ed9ac03b15c684226e613b923640ee07164e23
    (cherry picked from commit d3a57a9792a054b170adbeaa53248f0f354dbab2)

--- msm8909/sdm/libs/hwc2/hwc_display.cpp
+++ msm8909/sdm/libs/hwc2/hwc_display.cpp
@@ -1162,10 +1162,6 @@ HWC2::Error HWCDisplay::GetHdrCapabilities(uint32_t *out_num_types, int32_t *out
 
 
 HWC2::Error HWCDisplay::CommitLayerStack(void) {
-  if (shutdown_pending_ || layer_set_.empty()) {
-    return HWC2::Error::None;
-  }
-
   if (skip_validate_ && !CanSkipValidate()) {
     validated_.reset(type_);
   }
@@ -1173,6 +1169,10 @@ HWC2::Error HWCDisplay::CommitLayerStack(void) {
   if (!validated_.test(type_)) {
     DLOGV_IF(kTagClient, "Display %d is not validated", id_);
     return HWC2::Error::NotValidated;
+  }
+
+  if (shutdown_pending_ || layer_set_.empty()) {
+    return HWC2::Error::None;
   }
 
   DumpInputBuffers();
@@ -1264,8 +1264,11 @@ HWC2::Error HWCDisplay::PostCommitLayerStack(int32_t *out_retire_fence) {
       close(layer_buffer->acquire_fence_fd);
       layer_buffer->acquire_fence_fd = -1;
     }
+
+    layer->request.flags = {};
   }
 
+  client_target_->GetSDMLayer()->request.flags = {};
   *out_retire_fence = -1;
   if (!flush_) {
     // if swapinterval property is set to 0 then close and reset the list retire fence
@@ -1284,8 +1287,6 @@ HWC2::Error HWCDisplay::PostCommitLayerStack(int32_t *out_retire_fence) {
 
   geometry_changes_ = GeometryChanges::kNone;
   flush_ = false;
-
-  ClearRequestFlags();
 
   return status;
 }
@@ -1974,12 +1975,6 @@ void HWCDisplay::CloseAcquireFds() {
   if (client_target_acquire_fence >= 0) {
     close(client_target_acquire_fence);
     client_target_acquire_fence = -1;
-  }
-}
-
-void HWCDisplay::ClearRequestFlags() {
-  for (Layer *layer : layer_stack_.layers) {
-    layer->request.flags = {};
   }
 }
 
