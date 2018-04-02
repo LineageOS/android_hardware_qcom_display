b1013968b952f82abd64224d66e3da6159d036ed
Naseer Ahmed <naseer@codeaurora.org>
2018-04-02 17:28:51 -0400

    display: More fixes for VNDK
    Change-Id: I8887ffdde3d6f39f9405dd38e8eaedbb8da2a9b4

--- gralloc/service.cpp
+++ gralloc/service.cpp
@@ -26,7 +26,6 @@
  * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-#include <binder/ProcessState.h>
 #include <hidl/LegacySupport.h>
 #include "QtiAllocator.h"
 
