cfdb84fb5474c29e887efe0c34d1d00e54f5a060
D. Andrei Măceș <andrei@unlegacy-android.org>
2018-04-13 18:43:47 +0300

    display: Relax BOARD_USES_QCOM_HARDWARE dependency
    There's no need to guard Makefile inclusion at this point, when we're
    already using project paths. If need be, fold msm8974 with the legacy
    msm8960. This allows access for non-BOARD_USES_QCOM_HARDWARE devices.
    
    Change-Id: If013d4636aee814067f915d0da9b3f3d3f977cbf
    Signed-off-by: D. Andrei Măceș <andrei@unlegacy-android.org>

--- Android.mk
+++ Android.mk
@@ -6,13 +6,9 @@ display-hals += libmemtrack
 ifneq ($(TARGET_PROVIDES_LIBLIGHT),true)
 display-hals += liblight
 endif
-ifeq ($(call is-vendor-board-platform,QCOM),true)
-    include $(call all-named-subdir-makefiles,$(display-hals))
-else
-ifneq ($(filter msm8960,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter msm8960 msm8974,$(TARGET_BOARD_PLATFORM)),)
     #This is for mako since it doesn't have the QCOM make functions
     include $(call all-named-subdir-makefiles,$(display-hals))
-endif
 endif
 
 endif
