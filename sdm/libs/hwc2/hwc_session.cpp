7ac4b5544c9cf7d647e893a940ec91dd49e4d82b
Anjaneya Prasad Musunuri <aprasad@codeaurora.org>
2018-04-23 15:52:39 +0530

    hwc2: Reset FD limits to default.
    Remove change to increase fd limit as this change is redundant.
    This change reverts Ifd11521fe648204d8ce153a8010ad0f73cbfb9e8.
    
    Change-Id: I8331618e9c9b84053052986c91aba0c46fa6c86c
    CRs-Fixed: 2229346

--- sdm/libs/hwc2/hwc_session.cpp
+++ sdm/libs/hwc2/hwc_session.cpp
@@ -212,16 +212,6 @@ int HWCSession::Init() {
     return status;
   }
 
-  struct rlimit fd_limit = {};
-  getrlimit(RLIMIT_NOFILE, &fd_limit);
-  fd_limit.rlim_cur = fd_limit.rlim_cur * 2;
-  if (fd_limit.rlim_cur < fd_limit.rlim_max) {
-    auto err = setrlimit(RLIMIT_NOFILE, &fd_limit);
-    if (err) {
-      DLOGW("Unable to increase fd limit -  err:%d, %s", errno, strerror(errno));
-    }
-  }
-
   return 0;
 }
 
