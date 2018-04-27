2a419be2039a9ce066e0c2cc6c4e6b4170f2c9e0
Arun Kumar K.R <akumarkr@codeaurora.org>
2018-04-27 12:43:58 +0530

    sdm: Add support for WCG/HDR handling using SSPP
    - Handle HDR using SSPP when such capability is available.
    - For HW with SSPP tonemapping, color mode has to be set only
      when blend space is BT2020 and at the end of prepare.
    
    Change-Id: Ibe87c145addf01c6615bb7946a86ada8e6b9dddc
    Crs-fixed: 2234972

--- sdm/include/private/hw_info_types.h
+++ sdm/include/private/hw_info_types.h
@@ -612,6 +612,8 @@ struct HWLayersInfo {
   LayerStack *stack = NULL;        // Input layer stack. Set by the caller.
   uint32_t app_layer_count = 0;    // Total number of app layers. Must not be 0.
   uint32_t gpu_target_index = 0;   // GPU target layer index. 0 if not present.
+  std::vector<ColorPrimaries> wide_color_primaries = {};  // list of wide color primaries
+
   std::vector<Layer> hw_layers = {};  // Layers which need to be programmed on the HW
   std::vector<LayerExt> layer_exts = {};  // Extention layer having list of
                                           // exclusion rectangles for each layer
