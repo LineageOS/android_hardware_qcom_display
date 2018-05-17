84588fc9cdf66cfba07f28b5cb0cd20e46409098
Sushil Chauhan <sushilchauhan@codeaurora.org>
2018-05-17 12:13:07 -0700

    hwc2: Don't consider color mode count in setting HDR flag
    Don't consider color mode count while setting HDR flag on SDM layer.
    
    CRs-Fixed: 2244916
    Change-Id: Ie6642ba9df953f4b04f1af9761ac847be0b8a26c

--- sdm/libs/hwc2/hwc_display.cpp
+++ sdm/libs/hwc2/hwc_display.cpp
@@ -488,9 +488,6 @@ void HWCDisplay::BuildLayerStack() {
   metadata_refresh_rate_ = 0;
   layer_stack_.flags.animating = animating_;
 
-  uint32_t color_mode_count = 0;
-  display_intf_->GetColorModeCount(&color_mode_count);
-
   // Add one layer for fb target
   // TODO(user): Add blit target layers
   for (auto hwc_layer : layer_set_) {
@@ -556,7 +553,7 @@ void HWCDisplay::BuildLayerStack() {
     bool hdr_layer = layer->input_buffer.color_metadata.colorPrimaries == ColorPrimaries_BT2020 &&
                      (layer->input_buffer.color_metadata.transfer == Transfer_SMPTE_ST2084 ||
                      layer->input_buffer.color_metadata.transfer == Transfer_HLG);
-    if (hdr_layer && !disable_hdr_handling_ && color_mode_count) {
+    if (hdr_layer && !disable_hdr_handling_) {
       // dont honor HDR when its handling is disabled
       layer->input_buffer.flags.hdr = true;
       layer_stack_.flags.hdr_present = true;
