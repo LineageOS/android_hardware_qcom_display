ab75a7a355db56e1039bc1537b19912673afaf2d
Sushil Chauhan <sushilchauhan@codeaurora.org>
2018-05-23 15:25:52 -0700

    sdm: Clear fb_id map if it exceeds the size limit
    Clear the fb_id map of SDM HW layer, if it exceeds the size limit.
    It is required for cases like pre-rotation where new buffer handles
    are allocated and new unique handle ids are used on same HWC layer
    for consecutive draw cycles involving device rotations.
    
    CRs-Fixed: 2247747
    Change-Id: Iec71c42572f589717ad8f5677f84ff16e0aaaf01

--- sdm/libs/core/drm/hw_device_drm.cpp
+++ sdm/libs/core/drm/hw_device_drm.cpp
@@ -342,9 +342,10 @@ void HWDeviceDRM::Registry::MapBufferToFbId(Layer* layer, LayerBuffer* buffer) {
   }
 
   uint64_t handle_id = buffer->handle_id;
+  bool legacy_path = !handle_id || disable_fbid_cache_;
 
-  if (!handle_id || disable_fbid_cache_) {
-    // Legacy: Remove & Create fb_id in each frame
+  if (legacy_path || (layer->buffer_map->buffer_map.size() > fbid_cache_limit_)) {
+    // Clear fb_id map in each frame in legacy path or if the map size exceeds limit.
     layer->buffer_map->buffer_map.clear();
   }
 
@@ -367,8 +368,8 @@ void HWDeviceDRM::Registry::MapOutputBufferToFbId(LayerBuffer *output_buffer) {
 
   uint64_t handle_id = output_buffer->handle_id;
 
-  if (!handle_id || disable_fbid_cache_) {
-    // Legacy: Remove & Create fb_id in each frame
+  if (!handle_id || disable_fbid_cache_ || (output_buffer_map_.size() > fbid_cache_limit_)) {
+    // Clear output buffer map in each frame in legacy path or if the map size exceeds limit.
     output_buffer_map_.clear();
   }
 
