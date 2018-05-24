1e16c6d9b160b9314f9f74195674173f78b14c5e
Courtney Goeltzenleuchter <courtneygo@google.com>
2018-05-24 08:23:55 -0600

    Fix Buffer Overflow in Vendor Service display.qservice
    Bug: 63145942
    Test: adb shell vndservice call display.qservice 36 s16 sdlkfjsadlfkjasdf
    Change-Id: I3fdf5ccd2bf4ed0fa980883fefdb57eb5fbfeee7
    (cherry picked from commit 4050091844ccd427587024e5fd916113a5cc0029)

--- sdm/libs/hwc2/hwc_session.cpp
+++ sdm/libs/hwc2/hwc_session.cpp
@@ -1393,6 +1393,10 @@ android::status_t HWCSession::SetColorModeOverride(const android::Parcel *input_
   auto mode = static_cast<android_color_mode_t>(input_parcel->readInt32());
   auto device = static_cast<hwc2_device_t *>(this);
 
+  if (display > HWC_DISPLAY_VIRTUAL) {
+    return -EINVAL;
+  }
+
   auto err = CallDisplayFunction(device, display, &HWCDisplay::SetColorMode, mode);
   if (err != HWC2_ERROR_NONE)
     return -EINVAL;
