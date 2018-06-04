68970938a6a22cba37de031e01b9f0023475416a
Sushil Chauhan <sushilchauhan@codeaurora.org>
2018-06-04 13:19:00 -0700

    sdm: Reduce the fb_id cache limit for UI layers
    Reduce the fb_id cache limit for UI layers to 3 and for Rotator
    use cases to 2. This is required to reduce orphaned allocations
    in pre-rotation use cases.
    
    CRs-Fixed: 2253973
    Change-Id: I3989c0179d5a97b596605dd5405b89abbba5c509

--- sdm/libs/core/drm/hw_device_drm.cpp
+++ sdm/libs/core/drm/hw_device_drm.cpp
@@ -258,9 +258,11 @@ void HWDeviceDRM::Registry::Register(HWLayers *hw_layers) {
     LayerBuffer *input_buffer = &layer.input_buffer;
     HWRotatorSession *hw_rotator_session = &hw_layers->config[i].hw_rotator_session;
     HWRotateInfo *hw_rotate_info = &hw_rotator_session->hw_rotate_info[0];
+    fbid_cache_limit_ = input_buffer->flags.video ? VIDEO_FBID_LIMIT : UI_FBID_LIMIT;
 
     if (hw_rotator_session->mode == kRotatorOffline && hw_rotate_info->valid) {
       input_buffer = &hw_rotator_session->output_buffer;
+      fbid_cache_limit_ = ROTATOR_FBID_LIMIT;
     }
 
     MapBufferToFbId(&layer, input_buffer);
@@ -300,16 +302,20 @@ void HWDeviceDRM::Registry::MapBufferToFbId(Layer* layer, LayerBuffer* buffer) {
   }
 
   uint64_t handle_id = buffer->handle_id;
-  bool legacy_path = !handle_id || disable_fbid_cache_;
-
-  if (legacy_path || (layer->buffer_map->buffer_map.size() > fbid_cache_limit_)) {
-    // Clear fb_id map in each frame in legacy path or if the map size exceeds limit.
+  if (!handle_id || disable_fbid_cache_) {
+    // In legacy path, clear fb_id map in each frame.
     layer->buffer_map->buffer_map.clear();
-  }
+  } else {
 
-  if (layer->buffer_map->buffer_map.find(handle_id) != layer->buffer_map->buffer_map.end()) {
-    // Found fb_id for given handle_id key
-    return;
+    if (layer->buffer_map->buffer_map.find(handle_id) != layer->buffer_map->buffer_map.end()) {
+      // Found fb_id for given handle_id key
+      return;
+    }
+
+    if (layer->buffer_map->buffer_map.size() >= fbid_cache_limit_) {
+      // Clear fb_id map, if the size reaches cache limit.
+      layer->buffer_map->buffer_map.clear();
+    }
   }
 
   uint32_t fb_id = 0;
@@ -325,14 +331,19 @@ void HWDeviceDRM::Registry::MapOutputBufferToFbId(LayerBuffer *output_buffer) {
   }
 
   uint64_t handle_id = output_buffer->handle_id;
-
-  if (!handle_id || disable_fbid_cache_ || (output_buffer_map_.size() > fbid_cache_limit_)) {
-    // Clear output buffer map in each frame in legacy path or if the map size exceeds limit.
+  if (!handle_id || disable_fbid_cache_) {
+    // In legacy path, clear output buffer map in each frame.
     output_buffer_map_.clear();
-  }
+  } else {
 
-  if (output_buffer_map_.find(handle_id) != output_buffer_map_.end()) {
-    return;
+    if (output_buffer_map_.find(handle_id) != output_buffer_map_.end()) {
+      return;
+    }
+
+    if (output_buffer_map_.size() >= UI_FBID_LIMIT) {
+      // Clear output buffer map, if the size reaches cache limit.
+      output_buffer_map_.clear();
+    }
   }
 
   uint32_t fb_id = 0;
