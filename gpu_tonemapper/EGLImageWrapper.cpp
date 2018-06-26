ea4b3df304809297eb18cf6e594ad62106bf505c
siyuansixsixsix <siyuansixsixsix@users.noreply.github.com>
2018-06-26 12:49:02 +0800

    fix function deliver parameter

--- gpu_tonemapper/EGLImageWrapper.cpp
+++ gpu_tonemapper/EGLImageWrapper.cpp
@@ -123,7 +123,7 @@ static EGLImageBuffer* L_wrap(const private_handle_t *src)
     android::sp<android::GraphicBuffer> graphicBuffer =
         new android::GraphicBuffer(src->unaligned_width, src->unaligned_height, src->format,
 #ifndef __NOUGAT__
-                                   1, // Layer count
+#define __NOUGAT__, // Layer count
 #endif
                                    flags, src->width /*src->stride*/,
                                    native_handle, false);
