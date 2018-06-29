b1027af9b0290d2df4ee0a1e030004cc9545efdc
Dileep Marchya <dmarchya@codeaurora.org>
2018-06-29 10:50:16 +0530

    hwc2: Add support for screen refresh on second builtin display.
    Change-Id: I9b6a914a8c2d3427b0433eac93b0a0a0be12e261
    CRs-Fixed: 2200594

--- sdm/libs/hwc2/hwc_session.h
+++ sdm/libs/hwc2/hwc_session.h
@@ -286,6 +286,7 @@ class HWCSession : hwc2_device_t, HWCUEventListener, IDisplayConfig, public qCli
 
   android::status_t SetColorModeById(const android::Parcel *input_parcel);
   android::status_t getComposerStatus();
+  android::status_t RefreshScreen(const android::Parcel *input_parcel);
 
   void Refresh(hwc2_display_t display);
   void HotPlug(hwc2_display_t display, HWC2::Connection state);
