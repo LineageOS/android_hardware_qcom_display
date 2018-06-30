63fe9dc7f52335a43496b95870593124f92f7bd0
Aymen Bousselham <aymenbousselham@gmail.com>
2018-06-30 12:41:32 +0200

    libqdutils: Fix build with VNDK=current
    Change-Id: I106652edae9c52fa7311421c67f4ec0329bcf672

--- libqdutils/Android.mk
+++ libqdutils/Android.mk
@@ -4,6 +4,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE                  := libqdutils
 LOCAL_MODULE_TAGS             := optional
+LOCAL_HEADER_LIBRARIES        := gl_headers
 LOCAL_VENDOR_MODULE           := true
 LOCAL_SHARED_LIBRARIES        := $(common_libs) libui libbinder libqservice
 LOCAL_C_INCLUDES              := $(common_includes) $(kernel_includes)
