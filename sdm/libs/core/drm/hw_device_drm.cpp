ef00d16c58c49814fa9d4055f048b19c755d45ae
Alex Sarraf <asarraf@codeaurora.org>
2018-07-03 15:30:53 -0700

    sdm: Set mixer attributes with no check on resolution switch
    Previously we assumed destination scaling and DDIC/panel
    scaling could not coexist. Mixer resolution should still
    be updated when needed since connectors can support
    multiple modes which fails the check for resolution
    switching enabled.
    
    Change-Id: If97d6b0301677b4f951e6b2f3c23d9a3728d5635

--- sdm/libs/core/drm/hw_device_drm.cpp
+++ sdm/libs/core/drm/hw_device_drm.cpp
@@ -1612,10 +1612,6 @@ DisplayError HWDeviceDRM::UnsetScaleLutConfig() {
 }
 
 DisplayError HWDeviceDRM::SetMixerAttributes(const HWMixerAttributes &mixer_attributes) {
-  if (IsResolutionSwitchEnabled()) {
-    return kErrorNotSupported;
-  }
-
   if (!hw_resource_.hw_dest_scalar_info.count) {
     return kErrorNotSupported;
   }
