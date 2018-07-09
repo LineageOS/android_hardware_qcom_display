00192039bb0282a7ad7f54e3334d89983ed0bb68
Qing Huang <huangq@codeaurora.org>
2018-07-09 17:01:57 +0800

    display: Add default QDCM xml file support
    Add default QDCM xml file for SDM710
    
    Change-Id: I7063c3e554bb62d4a9f7d06cebef5e9e451393da

--- config/sdm710.mk
+++ config/sdm710.mk
@@ -18,6 +18,9 @@ PRODUCT_PACKAGES += \
     libqdMetaData \
     libqdMetaData.system
 
+#Qdcm calibration xml file for 2k panel
+PRODUCT_COPY_FILES += hardware/qcom/display/config/qdcm_calib_data_nt35597_cmd_mode_dsi_truly_panel_with_DSC.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_nt35597_cmd_mode_dsi_truly_panel_with_DSC.xml
+
 TARGET_FORCE_HWC_FOR_VIRTUAL_DISPLAYS := true
 MAX_VIRTUAL_DISPLAY_DIMENSION := 4096
 NUM_FRAMEBUFFER_SURFACE_BUFFERS := 2
