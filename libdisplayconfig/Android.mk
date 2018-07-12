2dc455d3803b8297f0b7394c442d0fd884a48c12
Luca Stefani <luca.stefani.ge1@gmail.com>
2018-07-12 22:14:28 +0200

    libdisplayconfig: Make DisplayConfig.h a module
    * Allows to build with VNDK current
    
    Change-Id: I1fa69233bdf7cd63fa39d174e2b51d7459be828f

--- libdisplayconfig/Android.mk
+++ libdisplayconfig/Android.mk
@@ -1,10 +1,14 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+LOCAL_COPY_HEADERS        := DisplayConfig.h
+LOCAL_VENDOR_MODULE       := true
+include $(BUILD_COPY_HEADERS)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE                  := libdisplayconfig
 LOCAL_MODULE_TAGS             := optional
 LOCAL_HEADER_LIBRARIES        := display_headers
-LOCAL_COPY_HEADERS            := DisplayConfig.h
 LOCAL_SRC_FILES               := DisplayConfig.cpp
 LOCAL_SHARED_LIBRARIES        := libhidlbase libhidltransport libutils \
                                  vendor.display.config@1.0
