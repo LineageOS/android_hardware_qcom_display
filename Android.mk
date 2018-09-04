59f93126ac9300c3e44b5a9915f4db3b454f85af
SagarMakhar <sagarmakhar@gmail.com>
2018-09-04 06:58:58 +0000

    display: include libqdutils and libqservice
    Change-Id: I3a90a90869048fad478a7ce53840b53a8f95f002

--- Android.mk
+++ Android.mk
@@ -1,7 +1,7 @@
 ifeq ($(call my-dir),$(call project-path-for,qcom-display))
 
 sdm-libs := sdm/libs
-display-hals := include $(sdm-libs)/utils $(sdm-libs)/core
+display-hals := include libqdutils libqservice $(sdm-libs)/utils $(sdm-libs)/core
 
 ifneq ($(TARGET_IS_HEADLESS), true)
     display-hals += libcopybit libmemtrack hdmi_cec \
