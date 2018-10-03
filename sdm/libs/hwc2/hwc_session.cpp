codeaurora-unoffical/platform-hardware-qcom-display

hwc2: Do not reset current validate while honoring refresh request
Do not reset validate in case of invalidate action

CRs-Fixed: 2246355
Change-Id: Ie6526afc4461714d56755d055301bee824689472

sdm/libs/hwc2/hwc_session.cpp
@@ -1514,6 +1514,7 @@ android::status_t HWCSession::QdcmCMDHandler(const android::Parcel *input_parcel
 
   int32_t action = pending_action.action;
   int count = -1;
+  bool invalidate_needed = true;
   while (action > 0) {
     count++;
     int32_t bit = (action & 1);
@@ -1525,7 +1526,10 @@ android::status_t HWCSession::QdcmCMDHandler(const android::Parcel *input_parcel
     DLOGV_IF(kTagQDCM, "pending action = %d", BITMAP(count));
     switch (BITMAP(count)) {
     case kInvalidating:
-      Refresh(HWC_DISPLAY_PRIMARY);
+      {
+        invalidate_needed = false;
+        Refresh(HWC_DISPLAY_PRIMARY);
+      }
       break;
     case kEnterQDCMMode:
       ret = color_mgr_->EnableQDCMMode(true, hwc_display_[HWC_DISPLAY_PRIMARY]);
@@ -1591,7 +1595,9 @@ android::status_t HWCSession::QdcmCMDHandler(const android::Parcel *input_parcel
   HWCColorManager::MarshallStructIntoParcel(resp_payload, output_parcel);
   req_payload.DestroyPayload();
   resp_payload.DestroyPayload();
-  hwc_display_[display_id]->ResetValidation();
+  if (invalidate_needed) {
+    hwc_display_[display_id]->ResetValidation();
+  }
 
   return ret;
 }

LineageOS/android_hardware_qcom_display

sdm/libs/hwc2/hwc_session.cpp
@@ -1709,6 +1709,7 @@ android::status_t HWCSession::QdcmCMDHandler(const android::Parcel *input_parcel
 
   int32_t action = pending_action.action;
   int count = -1;
+  bool invalidate_needed = true;
   while (action > 0) {
     count++;
     int32_t bit = (action & 1);
@@ -1720,7 +1721,10 @@ android::status_t HWCSession::QdcmCMDHandler(const android::Parcel *input_parcel
     DLOGV_IF(kTagQDCM, "pending action = %d, display_id = %d", BITMAP(count), display_id);
     switch (BITMAP(count)) {
     case kInvalidating:
-      Refresh(display_id);
+      {
+        invalidate_needed = false;
+        Refresh(display_id);
+      }
       break;
     case kEnterQDCMMode:
       ret = color_mgr_->EnableQDCMMode(true, hwc_display_[display_id]);
@@ -1821,7 +1825,9 @@ android::status_t HWCSession::QdcmCMDHandler(const android::Parcel *input_parcel
   HWCColorManager::MarshallStructIntoParcel(resp_payload, output_parcel);
   req_payload.DestroyPayload();
   resp_payload.DestroyPayload();
-  hwc_display_[display_id]->ResetValidation();
+  if (invalidate_needed) {
+    hwc_display_[display_id]->ResetValidation();
+  }
 
   return ret;
 }