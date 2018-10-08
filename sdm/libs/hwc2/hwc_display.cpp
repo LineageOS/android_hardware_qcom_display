984e7034bb86a65026b0fb829d887570808c61dc
Pullakavi Srinivas <spullaka@codeaurora.org>
2018-10-08 14:52:15 +0530

    hwc2: Reset validate on surface damage region update.
    Video mode panels make use of dirty regions to recompute
    aggregated fb roi that needs to be cropped from cached buffer.
    Reset validate to update appropriate rect.
    
    CRs-fixed: 2326636
    Change-Id: I9e6519ca833f84c36d9fa6fb8270ccf3d1fcf83d

--- sdm/libs/hwc2/hwc_display.cpp
+++ sdm/libs/hwc2/hwc_display.cpp
@@ -435,7 +435,7 @@ int HWCDisplay::Init() {
   DisplayConfigFixedInfo fixed_info = {};
   display_intf_->GetConfig(&fixed_info);
   is_cmd_mode_ = fixed_info.is_cmdmode;
-  partial_update_enabled_ = fixed_info.partial_update;
+  partial_update_enabled_ = fixed_info.partial_update || (!fixed_info.is_cmdmode);
   client_target_->SetPartialUpdate(partial_update_enabled_);
 
   DLOGI("Display created with id: %d", id_);
