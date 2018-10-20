b1f0d11f615ef21342b4cdc9c0917e3654a0f76f
Arne Coucheron <arco68@gmail.com>
2018-10-21 00:28:47 +0200

    display: Use generated kernel headers
    Change-Id: Ia1e22dc714f055c966c9ca297523ed33a8c92c8e

--- common.mk
+++ common.mk
@@ -42,9 +42,6 @@ ifeq ($(DISPLAY_DEBUG_SWAPINTERVAL),true)
     common_flags += -DDEBUG_SWAPINTERVAL
 endif
 
-common_deps  :=
-kernel_includes :=
-
 # Executed only on QCOM BSPs
 ifeq ($(TARGET_USES_QCOM_BSP),true)
 # Enable QCOM Display features
@@ -52,13 +49,4 @@ ifeq ($(TARGET_USES_QCOM_BSP),true)
 endif
 ifneq ($(call is-platform-sdk-version-at-least,18),true)
     common_flags += -DANDROID_JELLYBEAN_MR1=1
-endif
-ifeq ($(call is-vendor-board-platform,QCOM),true)
-# This check is to pick the kernel headers from the right location.
-# If the macro above is defined, we make the assumption that we have the kernel
-# available in the build tree.
-# If the macro is not present, the headers are picked from hardware/qcom/msmXXXX
-# failing which, they are picked from bionic.
-    common_deps += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
-    kernel_includes += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 endif
--- libhdmi/Android.mk
+++ libhdmi/Android.mk
@@ -5,10 +5,10 @@ include $(CLEAR_VARS)
 LOCAL_MODULE                  := libhdmi
 LOCAL_MODULE_TAGS             := optional
 LOCAL_VENDOR_MODULE           := true
-LOCAL_C_INCLUDES              := $(common_includes) $(kernel_includes)
+LOCAL_C_INCLUDES              := $(common_includes)
 LOCAL_SHARED_LIBRARIES        := $(common_libs) liboverlay libqdutils
 LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"qdhdmi\"
-LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps)
+LOCAL_HEADER_LIBRARIES        := generated_kernel_headers
 LOCAL_SRC_FILES               := hdmi.cpp
 
 include $(BUILD_SHARED_LIBRARY)
