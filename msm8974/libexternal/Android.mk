d757796da40c1921c19f567c0197770b5c8fb1b3
Arne Coucheron <arco68@gmail.com>
2018-10-21 00:28:47 +0200

    display: msm8974: Use generated kernel headers
    Change-Id: Ia1e22dc714f055c966c9ca297523ed33a8c92c8e

--- msm8974/libexternal/Android.mk
+++ msm8974/libexternal/Android.mk
@@ -5,10 +5,10 @@ include $(CLEAR_VARS)
 LOCAL_MODULE                  := libexternal
 LOCAL_MODULE_PATH             := $(TARGET_OUT_SHARED_LIBRARIES)
 LOCAL_MODULE_TAGS             := optional
-LOCAL_C_INCLUDES              := $(common_includes) $(kernel_includes)
+LOCAL_C_INCLUDES              := $(common_includes)
 LOCAL_SHARED_LIBRARIES        := $(common_libs) libmedia
 LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"qdexternal\"
-LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps)
+LOCAL_HEADER_LIBRARIES        := generated_kernel_headers
 LOCAL_SRC_FILES               := external.cpp
 
 include $(BUILD_SHARED_LIBRARY)
--- msm8974/libhwcomposer/Android.mk
+++ msm8974/libhwcomposer/Android.mk
@@ -8,13 +8,13 @@ LOCAL_MODULE                  := hwcomposer.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_RELATIVE_PATH    := hw
 LOCAL_PROPRIETARY_MODULE      := true
 LOCAL_MODULE_TAGS             := optional
-LOCAL_C_INCLUDES              := $(common_includes) $(kernel_includes)
+LOCAL_C_INCLUDES              := $(common_includes)
 LOCAL_SHARED_LIBRARIES        := $(common_libs) libEGL liboverlay \
                                  libexternal libqdutils libhardware_legacy \
                                  libdl libmemalloc libqservice libsync \
                                  libbinder libmedia
 LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"qdhwcomposer\"
-LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps)
+LOCAL_HEADER_LIBRARIES        := generated_kernel_headers
 LOCAL_SRC_FILES               := hwc.cpp          \
                                  hwc_utils.cpp    \
                                  hwc_uevents.cpp  \
