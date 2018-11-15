9b6b2f131e03dabb9941fa4776540481bc1c7b3f
Anjaneya Prasad Musunuri <aprasad@codeaurora.org>
2018-11-15 15:54:42 +0530

    hwc2: Fix compile errors in switch statement.
    Fix compile errors because of fall through switch statements.
    
    CRs-Fixed: 2352498
    
    Change-Id: I5ec641a2eeee1cfc4bf6750903c6287c5a137d9e

--- sdm/libs/hwc2/hwc_display.cpp
+++ sdm/libs/hwc2/hwc_display.cpp
@@ -1821,11 +1821,15 @@ int HWCDisplay::SetDisplayStatus(DisplayStatus display_status) {
   switch (display_status) {
     case kDisplayStatusResume:
       display_paused_ = false;
+      status = INT32(SetPowerMode(HWC2::PowerMode::On));
+      break;
     case kDisplayStatusOnline:
       status = INT32(SetPowerMode(HWC2::PowerMode::On));
       break;
     case kDisplayStatusPause:
       display_paused_ = true;
+      status = INT32(SetPowerMode(HWC2::PowerMode::Off));
+      break;
     case kDisplayStatusOffline:
       status = INT32(SetPowerMode(HWC2::PowerMode::Off));
       break;
