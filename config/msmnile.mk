bbb3b3b2f1552dccf3a435ac3abf4f5d10f848ab
Naseer Ahmed <naseer@codeaurora.org>
2018-11-22 14:06:47 -0500

    display: Switch to composer 2.3
    Change-Id: I153c2760ee5958a08bb452915b29d63abdd8639c

--- config/msmnile.mk
+++ config/msmnile.mk
@@ -1,8 +1,8 @@
 #Display related packages and configuration
 
 PRODUCT_PACKAGES += \
-    android.hardware.graphics.composer@2.2-impl \
-    android.hardware.graphics.composer@2.2-service \
+    android.hardware.graphics.composer@2.3-impl \
+    android.hardware.graphics.composer@2.3-service \
     android.hardware.graphics.mapper@2.0-impl-qti-display \
     vendor.qti.hardware.display.allocator@1.0-service \
     android.hardware.memtrack@1.0-impl \
