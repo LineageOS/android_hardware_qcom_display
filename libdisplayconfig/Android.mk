6dcec02f9e6a593dfc7bae0e66fd6b2e0e574550
David Ng <dave@codeaurora.org>
2018-12-06 12:49:57 -0800

    libdisplayconfig: Disable module for initial Kona bring-up
    Temporarily disable module for target bring-up.
    
    Change-Id: I56c8004c62e405442b57375a2e74a7afaf3a9efb

--- libdisplayconfig/Android.mk
+++ libdisplayconfig/Android.mk
@@ -1,5 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 
+ifneq ($(TARGET_BOARD_PLATFORM),kona)
 display_config_version := $(shell \
     if [ -d "$(TOP)/vendor/qcom/opensource/interfaces/display/config/1.4" ];\
     then echo DISPLAY_CONFIG_1_4; fi)
@@ -24,3 +25,4 @@ ifeq ($(LLVM_SA), true)
 endif
 
 include $(BUILD_SHARED_LIBRARY)
+endif
