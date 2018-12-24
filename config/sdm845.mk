eda4de15eeb000a05d19f8d894cc0309537d53f1
Subash Chander <subash@codeaurora.org>
2018-12-24 11:22:59 +0530

    display: Replace libqdmetadata.system with libqdmetadata
    With VNDK enablement, libqdmetadata is copying to both
    system and vendor partitions.
    
    CRs-Fixed: 2368161
    
    Change-Id: I135257092bf31ca0dcf10555f99a96711439bea7

--- config/sdm845.mk
+++ config/sdm845.mk
@@ -15,8 +15,7 @@ PRODUCT_PACKAGES += \
     hwcomposer.sdm845 \
     memtrack.sdm845 \
     libqdutils \
-    libqdMetaData \
-    libqdMetaData.system
+    libqdMetaData
 
 #Qdcm calibration xml file for 2k panel
 PRODUCT_COPY_FILES += hardware/qcom/display/config/qdcm_calib_data_nt35597_cmd_mode_dsi_truly_panel_with_DSC.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_nt35597_cmd_mode_dsi_truly_panel_with_DSC.xml
