f3a1f99be4428b59a393fc24fdf493a27bca4cf4
Mahesh Aia <mahaia@codeaurora.org>
2019-01-16 13:07:42 -0800

    hwc2: Fix handling of no metadata case for external
    In the case where video does not set refresh rate metadata, default to
    lower of all currently active (powered on) displays aka "throttling
    refresh rate"
    
    Change-Id: I696140d6b72199c113065ed032d2bb87bff90974
    CRs-Fixed: 2365041

--- sdm/libs/hwc2/hwc_display.cpp
+++ sdm/libs/hwc2/hwc_display.cpp
@@ -50,6 +50,8 @@
 
 namespace sdm {
 
+uint32_t HWCDisplay::throttling_refresh_rate_ = 60;
+
 bool NeedsToneMap(const LayerStack &layer_stack) {
   for (Layer *layer : layer_stack.layers) {
     if (layer->request.flags.tone_map) {
@@ -1148,7 +1150,7 @@ HWC2::Error HWCDisplay::SetClientTarget(buffer_handle_t target, int32_t acquire_
   }
 
   Layer *sdm_layer = client_target_->GetSDMLayer();
-  sdm_layer->frame_rate = current_refresh_rate_;
+  sdm_layer->frame_rate = std::min(current_refresh_rate_, HWCDisplay::GetThrottlingRefreshRate());
   client_target_->SetLayerSurfaceDamage(damage);
   int translated_dataspace = TranslateFromLegacyDataspace(dataspace);
   if (client_target_->GetLayerDataspace() != translated_dataspace) {
@@ -2296,7 +2298,7 @@ void HWCDisplay::UpdateRefreshRate() {
       continue;
     }
     auto layer = hwc_layer->GetSDMLayer();
-    layer->frame_rate = current_refresh_rate_;
+    layer->frame_rate = std::min(current_refresh_rate_, HWCDisplay::GetThrottlingRefreshRate());
   }
 }
 
