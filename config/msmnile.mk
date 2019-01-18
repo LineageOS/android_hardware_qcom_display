4f95fff7b10e684686670a2c5d3937e83dd3687c
Naseer Ahmed <naseer@codeaurora.org>
2019-01-17 19:26:37 -0500

    display: Do not use target names in hw modules
    Change-Id: Idf57c3a02330cbd79b64de6512493aedc23c6d08
    CRs-Fixed: 2385459

--- config/msmnile.mk
+++ config/msmnile.mk
@@ -9,10 +9,10 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-service \
     android.hardware.light@2.0-impl \
     android.hardware.light@2.0-service \
-    gralloc.msmnile \
-    lights.msmnile \
-    hwcomposer.msmnile \
-    memtrack.msmnile \
+    gralloc.qcom \
+    lights.qcom \
+    hwcomposer.qcom \
+    memtrack.qcom \
     libqdutils \
     libqdMetaData \
     libqdMetaData.system \
