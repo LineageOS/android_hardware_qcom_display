7e14ffc7347b699dcda64371b1284ef392b68797
Naseer Ahmed <naseer@codeaurora.org>
2019-01-28 16:22:16 -0500

    display: Remove unused headers
    These headers have moved to vendor/qcom/opensource/commonsys-intf
    
    Change-Id: Idf6ee892b996bbc5050c178639b21ef459f4aaf9

--- include/Android.mk
+++ include/Android.mk
@@ -7,11 +7,9 @@ include $(CLEAR_VARS)
 # LOCAL_HEADER_LIBRARIES := display_headers
 LOCAL_VENDOR_MODULE           := true
 LOCAL_COPY_HEADERS_TO         := $(common_header_export_path)
-LOCAL_COPY_HEADERS            := color_metadata.h \
-                                 display_color_processing.h \
+LOCAL_COPY_HEADERS            := display_color_processing.h \
                                  display_properties.h \
                                  ../libqdutils/qd_utils.h \
-                                 ../libqdutils/qdMetaData.h \
                                  ../libqdutils/display_config.h \
                                  ../libqservice/QServiceUtils.h \
                                  ../libqservice/IQService.h \
