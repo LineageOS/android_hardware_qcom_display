7082e1e83e569cc44c281d96e7d67df78b264a5d
Xiaowen Wu <wxiaowen@codeaurora.org>
2019-02-06 12:31:07 -0500

    sdm/hwc: add display indices property for client id init
    Add display indices property for aosp build without ExSurfaceFlinger,
    to allow special display hwcId generated by vendor HWC for each type
    of displays.
    
    Change-Id: I389c98c3e54c6280597b2a6b7a63d624a799310f

--- include/display_properties.h
+++ include/display_properties.h
@@ -114,6 +114,9 @@
 #define PREFER_MULTIRECT_PROP                DISPLAY_PROP("prefer_multirect")
 #define DROP_SKEWED_VSYNC                    DISPLAY_PROP("drop_skewed_vsync")
 #define DISABLE_FAST_PATH                    DISPLAY_PROP("disable_fast_path")
+#define BUILTIN_BASEID_AND_SIZE_PROP         DISPLAY_PROP("builtin_baseid_and_size")
+#define PLUGGABLE_BASEID_AND_SIZE_PROP       DISPLAY_PROP("pluggable_baseid_and_size")
+#define VIRTUAL_BASEID_AND_SIZE_PROP         DISPLAY_PROP("virtual_baseid_and_size")
 
 #define ZERO_SWAP_INTERVAL                   "vendor.debug.egl.swapinterval"
 
