3d771f186c8864d99b543d1dfb81240191a8af87
Rajavenu Kyatham <rkyatham@codeaurora.org>
2019-02-13 12:25:29 +0530

    hwc2: Switch vsync among displays in normal mode.
    - Display panel may be reconfigured to operate in lower
      fps during doze mode, so vsync events may not arrive
      at 60fps. Switch over to next active display when an
      active display goes in doze mode.
    
    CRs-Fixed: 2325564
    Change-Id: Id646f3fd2303cb1f3900e4358ae7967214bdef91

--- sdm/libs/hwc2/hwc_session.cpp
+++ sdm/libs/hwc2/hwc_session.cpp
@@ -3091,7 +3091,7 @@ hwc2_display_t HWCSession::GetNextVsyncSource() {
       continue;
     }
 
-    if (hwc_display->GetCurrentPowerMode() != HWC2::PowerMode::Off) {
+    if (hwc_display->GetCurrentPowerMode() == HWC2::PowerMode::On) {
       return info.client_id;
     }
   }
