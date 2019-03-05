e63d95d36bb6072863c6d8b155b084460157fdd9
Padmanabhan Komanduru <pkomandu@codeaurora.org>
2019-03-05 11:25:00 +0530

    hwc2: Trigger two refresh requests on doze state transition.
    On first refresh update power mode.
    On second refresh update any settings that need to be set post
    power mode transition. Ex: PP features.
    
    CRs-Fixed: 2348944
    Change-Id: I7a3e62d9d4af05637b1a0f3f43bfd9d39f6faf3f

--- sdm/libs/hwc2/hwc_session.cpp
+++ sdm/libs/hwc2/hwc_session.cpp
@@ -732,8 +732,24 @@ int32_t HWCSession::PresentDisplay(hwc2_device_t *device, hwc2_display_t display
 
   hwc_session->HandlePowerOnPending(display, *out_retire_fence);
   hwc_session->HandleHotplugPending(display, *out_retire_fence);
+  hwc_session->HandlePendingRefresh();
 
   return INT32(status);
+}
+
+void HWCSession::HandlePendingRefresh() {
+  if (pending_refresh_.none()) {
+    return;
+  }
+
+  for (size_t i = 0; i < pending_refresh_.size(); i++) {
+    if (pending_refresh_.test(i)) {
+      Refresh(i);
+    }
+    break;
+  }
+
+  pending_refresh_.reset();
 }
 
 int32_t HWCSession::RegisterCallback(hwc2_device_t *device, int32_t descriptor,
@@ -978,6 +994,8 @@ int32_t HWCSession::SetPowerMode(hwc2_device_t *device, hwc2_display_t display, 
   // Trigger refresh for doze mode to take effect.
   if (mode == HWC2::PowerMode::Doze) {
     hwc_session->Refresh(display);
+    // Trigger one more refresh for PP features to take effect.
+    hwc_session->pending_refresh_.set(UINT32(display));
   }
 
   return HWC2_ERROR_NONE;
