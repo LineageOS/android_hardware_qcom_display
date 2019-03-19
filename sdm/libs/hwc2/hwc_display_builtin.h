238166cf3a5085e3e6e2adb013f1dd3927eb8318
Padmanabhan Komanduru <pkomandu@codeaurora.org>
2019-03-19 14:30:48 +0530

    hwc2: rename the enable_drop_refresh property to enable_optimize_refresh
    Add change to rename the enable_drop_refresh property to
    enable_optimize_refresh for better readability.
    
    Change-Id: I379b7ef610bc0979b9a78dac607f371acfe24fbc
    CRs-Fixed: 2418804

--- sdm/libs/hwc2/hwc_display_builtin.h
+++ sdm/libs/hwc2/hwc_display_builtin.h
@@ -129,7 +129,7 @@ class HWCDisplayBuiltIn : public HWCDisplay {
   void *output_buffer_base_ = nullptr;
   int default_mode_status_ = 0;
   bool pending_refresh_ = true;
-  bool enable_drop_refresh_ = false;
+  bool enable_optimize_refresh_ = false;
   bool hdr_present_ = false;
 
   // Members for 1 frame capture in a client provided buffer
