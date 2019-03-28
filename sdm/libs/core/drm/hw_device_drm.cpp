10bcb5494e8d5bf8a53fa6a3d4aecf64ebc51697
Rajavenu Kyatham <rkyatham@codeaurora.org>
2019-03-28 11:48:24 +0530

    Revert "sdm: Set mixer attributes with no check on resolution switch"
    This reverts commit ef00d16c58c49814fa9d4055f048b19c755d45ae.
    
    Change-Id: I3ab97840ec5b1a69d2632e6e82949737bef83720
    CRs-fixed: 2419453

--- sdm/libs/core/drm/hw_device_drm.cpp
+++ sdm/libs/core/drm/hw_device_drm.cpp
@@ -1747,6 +1747,10 @@ DisplayError HWDeviceDRM::UnsetScaleLutConfig() {
 }
 
 DisplayError HWDeviceDRM::SetMixerAttributes(const HWMixerAttributes &mixer_attributes) {
+  if (IsResolutionSwitchEnabled()) {
+    return kErrorNotSupported;
+  }
+
   if (!hw_resource_.hw_dest_scalar_info.count) {
     return kErrorNotSupported;
   }
