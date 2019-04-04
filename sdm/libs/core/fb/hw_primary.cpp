b12aee98823c7e04ea2d8a80ca5f74fed86bce89
Namit Solanki <nsolanki@codeaurora.org>
2019-04-04 13:06:35 +0530

    sdm: Add support for destination scaler
    - Use mixer_resolution property to enable
      Destination scaler.
    - Mixer resolution is informbed to SF as FB
      resolution.
    - If mixer resolution does not match to any
      supported TV res. , use closest supported
      TV resolution.
    - Do not fail mixer res. update. If mixer
      resolution ratio do not match to display
      resolution ratio, update mixer resolution
      accordingly.
    - Apply mix cap of mixer res. to user provided
      mixer resolution.
    
    Change-Id: I3aa5c3aeee71962da2d893cbe5b02eb5ea4dd876

--- sdm/libs/core/fb/hw_primary.cpp
+++ sdm/libs/core/fb/hw_primary.cpp
@@ -652,7 +652,7 @@ DisplayError HWPrimary::SetPPFeatures(PPFeaturesConfig *feature_list) {
   return kErrorNone;
 }
 
-DisplayError HWPrimary::SetMixerAttributes(const HWMixerAttributes &mixer_attributes) {
+DisplayError HWPrimary::SetMixerAttributes(HWMixerAttributes &mixer_attributes) {
   if (IsResolutionSwitchEnabled()) {
     return kErrorNotSupported;
   }
@@ -729,6 +729,11 @@ DisplayError HWPrimary::GetDynamicDSIClock(uint64_t *bitclk) {
 
   *bitclk = UINT64(atoi(dsi_clk));
   Sys::close_(fd);
+  return kErrorNone;
+}
+
+DisplayError HWPrimary::SetActiveConfig(uint32_t active_config) {
+  active_config_index_ = active_config;
   return kErrorNone;
 }
 
