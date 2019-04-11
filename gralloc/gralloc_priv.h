87df22c93158be959b301d4aa3354da52a75db7b
Ashish Kumar <asku@codeaurora.org>
2019-04-11 14:26:05 +0530

    commonsys-intf: display: Add new camera formats
    Add pixel formats which are used by camera in gralloc_priv.h
    
    CRs-Fixed: 2460241
    Change-Id: I905eeef806c74c623a6f902277d3cf8168b2a6dd

--- gralloc/gralloc_priv.h
+++ gralloc/gralloc_priv.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2018, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2011-2019, The Linux Foundation. All rights reserved.
  * Not a Contribution
  *
  * Copyright (C) 2008 The Android Open Source Project
@@ -154,6 +154,11 @@ inline int roundUpToPageSize(int x) {
 #define HAL_PIXEL_FORMAT_BGR_888 0x121
 
 #define HAL_PIXEL_FORMAT_INTERLACE 0x180
+
+// Camera utils format
+#define HAL_PIXEL_FORMAT_NV12_LINEAR_FLEX  0x125
+#define HAL_PIXEL_FORMAT_NV12_UBWC_FLEX 0x126
+#define HAL_PIXEL_FORMAT_MULTIPLANAR_FLEX 0x127
 
 // v4l2_fourcc('Y', 'U', 'Y', 'L'). 24 bpp YUYV 4:2:2 10 bit per component
 #define HAL_PIXEL_FORMAT_YCbCr_422_I_10BIT 0x4C595559
