4625ffba6c8ec7606b37849c7331180d2e186ce5
c_xshu <xshu@codeaurora.org>
2019-04-15 18:11:18 +0800

    display: Update new qdcm xml file on trinket
    Add the new qdcm xml file on trinket.
    
    Change-Id: I45aa30c2ffc5018cdadfe615d0c1ae47919bacb9

--- config/trinket.mk
+++ config/trinket.mk
@@ -18,9 +18,11 @@ PRODUCT_PACKAGES += \
     libqdMetaData.system \
     modetest
 
-#QDCM calibration xml file
+#QDCM calibration xml file base on td4330
 PRODUCT_COPY_FILES += hardware/qcom/display/config/qdcm_calib_data_trinket_default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_td4330_cmd_mode_dsi_truly_panel.xml
 PRODUCT_COPY_FILES += hardware/qcom/display/config/qdcm_calib_data_trinket_default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_td4330_video_mode_dsi_truly_panel.xml
+#QDCM calibration xml file base on nt36672
+PRODUCT_COPY_FILES += hardware/qcom/display/config/qdcm_calib_data_trinket_default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_nt36672_truly_fhd_video_mode_dsi_panel.xml
 
 #Enable Charging Icon
 TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
