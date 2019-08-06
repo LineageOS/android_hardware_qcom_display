62ad020982facdb487f5bd979bd916c86792c4cf
z3DD3r <z3dd3r@gmail.com>
2019-08-06 19:57:59 +0300

    libcopybit: fix compilation for msm8974
    fixesfollowing errors:
    ./msm8974/libcopybit/software_converter.cpp: error: use of undeclared identifier 'memcpy'
    ./msm8974/libcopybit/copybit_c2d.cpp: error: use of GNU old-style field designator extension [-Werror,-Wgnu-designator]
    
    Change-Id: I51f56c488ad1c8e725f81260a26ee3f0572a3bc9

--- msm8974/libcopybit/Android.mk
+++ msm8974/libcopybit/Android.mk
@@ -27,7 +27,7 @@ LOCAL_PROPRIETARY_MODULE      := true
 LOCAL_MODULE_TAGS             := optional
 LOCAL_C_INCLUDES              := $(common_includes)
 LOCAL_SHARED_LIBRARIES        := $(common_libs) libdl libmemalloc
-LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"qdcopybit\"
+LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"qdcopybit\" -Wno-gnu-designator
 LOCAL_HEADER_LIBRARIES        := generated_kernel_headers
 
 ifeq ($(TARGET_USES_C2D_COMPOSITION),true)
