865761eb3881616b6245fb44055bf9b80da01204
dianlujitao <dianlujitao@lineageos.org>
2019-09-06 11:19:55 +0800

    display: Use generated kernel headers
    Change-Id: I7cdd8c2ac14597ed0ddd458de5a6ee9ceff89380

--- gpu_tonemapper/Android.mk
+++ gpu_tonemapper/Android.mk
@@ -12,9 +12,8 @@ LOCAL_MODULE              := libgpu_tonemapper
 LOCAL_VENDOR_MODULE       := true
 LOCAL_MODULE_TAGS         := optional
 LOCAL_C_INCLUDES          := $(TARGET_OUT_HEADERS)/qcom/display/
-LOCAL_C_INCLUDES          += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 LOCAL_SHARED_LIBRARIES    := libEGL libGLESv2 libGLESv3 libui libutils liblog
-LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps) $(kernel_deps)
+LOCAL_HEADER_LIBRARIES    := generated_kernel_headers
 
 LOCAL_CFLAGS              := $(version_flag) -Wno-missing-field-initializers -Wall \
                              -Wno-unused-parameter -std=c++11 -DLOG_TAG=\"GPU_TONEMAPPER\"
