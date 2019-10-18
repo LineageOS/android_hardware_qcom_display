5e715915c4b101c8066b424d18bc14329aa7b5fe
Ramakant Singh <ramakant@codeaurora.org>
2019-10-18 10:34:35 +0530

    display: Remove C++ standard flags
    Some headers included by display modules require c++17 now in newer
    builds. Remove the forced standard setting to let the build pick
    the latest C++ version
    
    Change-Id: I76f9e0c03ce08100c3dd7caf5932eaa941fff0c8

--- libdebug/Android.mk
+++ libdebug/Android.mk
@@ -5,7 +5,7 @@ LOCAL_MODULE                  := libdisplaydebug
 LOCAL_VENDOR_MODULE           := true
 LOCAL_MODULE_TAGS             := optional
 LOCAL_SHARED_LIBRARIES        := libdl
-LOCAL_CFLAGS                  := -DLOG_TAG=\"SDM\" -Wall -std=c++11 -Werror -fno-operator-names
+LOCAL_CFLAGS                  := -DLOG_TAG=\"SDM\" -Wall -Werror -fno-operator-names
 LOCAL_CLANG                   := true
 LOCAL_SRC_FILES               := debug_handler.cpp
 LOCAL_COPY_HEADERS_TO         := qcom/display
