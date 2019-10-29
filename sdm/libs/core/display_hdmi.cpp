a73730bd6c0c154f060ac40aeb6a18533f42cbeb
Namit Solanki <nsolanki@codeaurora.org>
2019-10-29 16:42:57 +0530

    sdm : Do not enable dest scale for HDMI as secondary
    Change-Id: I7d436d5504605ce189a18597f155fc133f576ece

--- sdm/libs/core/display_hdmi.cpp
+++ sdm/libs/core/display_hdmi.cpp
@@ -160,10 +160,9 @@ DisplayError DisplayHDMI::Prepare(LayerStack *layer_stack) {
   uint32_t display_width = display_attributes_.x_pixels;
   uint32_t display_height = display_attributes_.y_pixels;
 
-  if (NeedsMixerReconfiguration(layer_stack, &new_mixer_width, &new_mixer_height)) {
-    if (dest_scale_enabled_) {
-      CheckMinMixerResolution(&new_mixer_width, &new_mixer_height);
-    }
+  if (dest_scale_enabled_ && NeedsMixerReconfiguration(layer_stack, &new_mixer_width,
+      &new_mixer_height)) {
+    CheckMinMixerResolution(&new_mixer_width, &new_mixer_height);
     error = ReconfigureMixer(new_mixer_width, new_mixer_height);
     if (error != kErrorNone) {
       ReconfigureMixer(display_width, display_height);
