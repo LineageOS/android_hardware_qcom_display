5b259fbc6d62de692d716b5b0268c4373d2ddac1
Naseer Ahmed <naseer@codeaurora.org>
2019-11-28 18:39:44 -0500

    display: Disable gralloc module
    This module is no longer supported. IMapper and IAllocator
    APIs must be used instead
    
    Change-Id: I0c4c459c6c4acf06f9ae2c4257f8da857bdfb628

--- config/display-product.mk
+++ config/display-product.mk
@@ -7,7 +7,6 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-service \
     android.hardware.light@2.0-impl \
     android.hardware.light@2.0-service \
-    gralloc.$(TARGET_BOARD_PLATFORM) \
     lights.$(TARGET_BOARD_PLATFORM) \
     hwcomposer.$(TARGET_BOARD_PLATFORM) \
     memtrack.$(TARGET_BOARD_PLATFORM) \
--- gralloc/Android.mk
+++ gralloc/Android.mk
@@ -12,25 +12,6 @@ endif
 include $(LOCAL_PATH)/../common.mk
 include $(CLEAR_VARS)
 
-LOCAL_MODULE                  := gralloc.$(TARGET_BOARD_PLATFORM)
-LOCAL_SANITIZE                := integer_overflow
-LOCAL_VENDOR_MODULE           := true
-LOCAL_MODULE_RELATIVE_PATH    := hw
-LOCAL_MODULE_TAGS             := optional
-LOCAL_C_INCLUDES              := $(common_includes) $(kernel_includes)
-
-LOCAL_HEADER_LIBRARIES        := display_headers
-LOCAL_SHARED_LIBRARIES        := $(common_libs) libqdMetaData libsync libgrallocutils \
-                                 libgralloccore \
-                                 android.hardware.graphics.mapper@2.0 \
-                                 android.hardware.graphics.mapper@2.1 \
-                                 android.hardware.graphics.mapper@3.0
-LOCAL_CFLAGS                  := $(common_flags) $(qmaa_flags) -DLOG_TAG=\"qdgralloc\" -Wall -Werror
-LOCAL_CLANG                   := true
-LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps)
-LOCAL_SRC_FILES               := gr_device_impl.cpp
-include $(BUILD_SHARED_LIBRARY)
-
 #libgrallocutils
 include $(CLEAR_VARS)
 LOCAL_MODULE                  := libgrallocutils
