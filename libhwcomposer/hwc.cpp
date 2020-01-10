e8cc8e0e8c36d36b93fd85a7bf5fedc7cbb834d5
Arne Coucheron <arco68@gmail.com>
2020-01-10 15:56:47 +0100

    hwc: Iterate through numDisplays in reset function
    A recent CAF addition to hwcomposer_defs.h added new
    display types, and this exposed a bug in our hwcomposer
    which causes a crash in surfaceflinger. Fix this by
    iterating through numDisplays instead of HWC_NUM_DISPLAY_TYPES.
    This aligns with newer CAF platforms.
    
    Change-Id: Iafb506e6b7b3551c4504386c3ced75fa550a65cb

--- libhwcomposer/hwc.cpp
+++ libhwcomposer/hwc.cpp
@@ -218,8 +218,13 @@ static bool isHotPluggable(hwc_context_t *ctx, int dpy) {
 
 static void reset(hwc_context_t *ctx, int numDisplays,
                   hwc_display_contents_1_t** displays) {
+<<<<<<< HEAD   (0760f5 Make hwc_vsync thread realtime)
 
 
+=======
+    ctx->isPaddingRound = false;
+    memset(ctx->listStats, 0, sizeof(ctx->listStats));
+>>>>>>> CHANGE (d9308f hwc: Iterate through numDisplays in reset function)
     for(int i = 0; i < numDisplays; i++) {
         hwc_display_contents_1_t *list = displays[i];
         // XXX:SurfaceFlinger no longer guarantees that this
