ee22c1cefbe66981b7ebcacd40c1cfa3b57b13a6
Namit Solanki <nsolanki@codeaurora.org>
2020-01-22 11:43:55 +0530

    sdm : Remove compilation dependency from driver for YUV formats.
    Change-Id: Ib978b4d81a260b5183a6c5742bbaf32a9c24ef44

--- sdm/libs/core/drm/hw_device_drm.cpp
+++ sdm/libs/core/drm/hw_device_drm.cpp
@@ -74,6 +74,12 @@
 #ifndef DRM_FORMAT_MOD_QCOM_TIGHT
 #define DRM_FORMAT_MOD_QCOM_TIGHT fourcc_mod_code(QCOM, 0x4)
 #endif
+#ifndef DRM_MODE_FLAG_SUPPORTS_YUV422
+#define DRM_MODE_FLAG_SUPPORTS_YUV422 (1<<21)
+#endif
+#ifndef DRM_MODE_FLAG_SUPPORTS_YUV420
+#define DRM_MODE_FLAG_SUPPORTS_YUV420 (1<<22)
+#endif
 
 using std::string;
 using std::to_string;
