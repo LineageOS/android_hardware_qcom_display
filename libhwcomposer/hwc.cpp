cile381/platform-hardware-qcom-display

hwc: wait for commit finish when unset overlay
When there is unset overlay in prepare, commit needs to be sync
mode and wait for VSYNC. Otherwise, kernel resources may not be
ready for next overlay set.

Change-Id: Ibfc64ddd035993781eca2b4393650ce716d87549

libhwcomposer/hwc.cpp
@@ -557,7 +557,8 @@ static int hwc_set_primary(hwc_context_t *ctx, hwc_display_contents_1_t* list) {
             }
         }
 
-        if(!Overlay::displayCommit(ctx->dpyAttr[dpy].fd)) {
+        if(!Overlay::displayCommit(ctx->dpyAttr[dpy].arb_fd,
+            ctx->mOverlay->waitForCommitFinish())) {
             ALOGE("%s: display commit fail for %d dpy!", __FUNCTION__, dpy);
             ret = -1;
         }
@@ -613,7 +614,8 @@ static int hwc_set_external(hwc_context_t *ctx,
             }
         }
 
-        if(!Overlay::displayCommit(ctx->dpyAttr[dpy].fd)) {
+        if(!Overlay::displayCommit(ctx->dpyAttr[dpy].arb_fd,
+            ctx->mOverlay->waitForCommitFinish())) {
             ALOGE("%s: display commit fail for %d dpy!", __FUNCTION__, dpy);
             ret = -1;
         }
@@ -669,7 +671,8 @@ static int hwc_set_virtual(hwc_context_t *ctx,
             }
         }
 
-        if(!Overlay::displayCommit(ctx->dpyAttr[dpy].fd)) {
+        if(!Overlay::displayCommit(ctx->dpyAttr[dpy].arb_fd,
+            ctx->mOverlay->waitForCommitFinish())) {
             ALOGE("%s: display commit fail for %d dpy!", __FUNCTION__, dpy);
             ret = -1;
         }

LineageOS/android_hardware_qcom_display

libhwcomposer/hwc.cpp
@@ -566,7 +566,8 @@ static int hwc_set_primary(hwc_context_t *ctx, hwc_display_contents_1_t* list) {
             }
         }
 
-        if(!Overlay::displayCommit(ctx->dpyAttr[dpy].fd)) {
+        if(!Overlay::displayCommit(ctx->dpyAttr[dpy].fd,
+            ctx->mOverlay->waitForCommitFinish())) {
             ALOGE("%s: display commit fail for %d dpy!", __FUNCTION__, dpy);
             ret = -1;
         }
@@ -619,7 +620,8 @@ static int hwc_set_external(hwc_context_t *ctx,
             }
         }
 
-        if(!Overlay::displayCommit(ctx->dpyAttr[dpy].fd)) {
+        if(!Overlay::displayCommit(ctx->dpyAttr[dpy].fd,
+            ctx->mOverlay->waitForCommitFinish())) {
             ALOGE("%s: display commit fail for %d dpy!", __FUNCTION__, dpy);
             ret = -1;
         }
@@ -669,7 +671,8 @@ static int hwc_set_virtual(hwc_context_t *ctx,
             }
         }
 
-        if(!Overlay::displayCommit(ctx->dpyAttr[dpy].fd)) {
+        if(!Overlay::displayCommit(ctx->dpyAttr[dpy].fd,
+            ctx->mOverlay->waitForCommitFinish())) {
             ALOGE("%s: display commit fail for %d dpy!", __FUNCTION__, dpy);
             ret = -1;
         }