2b7dc8c4b12069b10f208cc5a054b3ce728242dc
Demon000 <demonsingur@gmail.com>
2020-05-21 01:40:04 +0300

    sdm: mark FOD pressed layer by setting a bit on ZPOS
    Change-Id: Ie1503da41766c31c9ec31bbb4282ae9ed62defce

--- sdm/libs/hwc2/hwc_layers.h
+++ sdm/libs/hwc2/hwc_layers.h
@@ -119,6 +119,9 @@ class HWCLayer {
   void SetLayerAsMask();
   bool BufferLatched() { return buffer_flipped_; }
   void ResetBufferFlip() { buffer_flipped_ = false; }
+#ifdef FOD_ZPOS
+  bool IsFodPressed() { return fod_pressed_; }
+#endif
 
  private:
   Layer *layer_ = nullptr;
@@ -139,6 +142,9 @@ class HWCLayer {
   bool non_integral_source_crop_ = false;
   bool has_metadata_refresh_rate_ = false;
   bool buffer_flipped_ = false;
+#ifdef FOD_ZPOS
+  bool fod_pressed_ = false;
+#endif
 
   // Composition requested by client(SF) Original
   HWC2::Composition client_requested_orig_ = HWC2::Composition::Device;
