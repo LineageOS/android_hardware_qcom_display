7b1c3cc76d87a20ce8845519cf818d44bc481644
Demon000 <demonsingur@gmail.com>
2020-05-21 01:40:04 +0300

    sdm: mark FOD pressed layer by setting a bit on ZPOS
    Change-Id: Ie1503da41766c31c9ec31bbb4282ae9ed62defce

--- sdm/libs/hwc2/Android.mk
+++ sdm/libs/hwc2/Android.mk
@@ -9,6 +9,7 @@ LOCAL_VENDOR_MODULE           := true
 LOCAL_MODULE_RELATIVE_PATH    := hw
 LOCAL_MODULE_TAGS             := optional
 LOCAL_C_INCLUDES              := $(common_includes)
+LOCAL_C_INCLUDES              += $(kernel_includes)
 LOCAL_HEADER_LIBRARIES        := display_headers
 
 LOCAL_CFLAGS                  := -Wno-missing-field-initializers -Wno-unused-parameter \
@@ -26,6 +27,9 @@ LOCAL_SHARED_LIBRARIES        := libsdmcore libqservice libbinder libhardware li
                                  android.hardware.graphics.allocator@2.0 \
                                  android.hardware.graphics.composer@2.2 \
 
+ifeq ($(TARGET_USES_FOD_ZPOS), true)
+LOCAL_CFLAGS                  += -DFOD_ZPOS
+endif
 
 LOCAL_SRC_FILES               := hwc_session.cpp \
                                  hwc_session_services.cpp \
