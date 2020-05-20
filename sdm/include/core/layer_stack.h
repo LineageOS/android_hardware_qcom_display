b2bba1ec42b33b282c64af7622e8244d15e91719
Demon000 <demonsingur@gmail.com>
2020-05-21 01:40:04 +0300

    sdm: mark FOD pressed layer by setting a bit on ZPOS
    [Ayrton] Adapt for sm8250
    
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
    Change-Id: Ie1503da41766c31c9ec31bbb4282ae9ed62defce

--- sdm/include/core/layer_stack.h
+++ sdm/include/core/layer_stack.h
@@ -44,6 +44,10 @@
 #include "layer_buffer.h"
 #include "sdm_types.h"
 
+#ifdef FOD_ZPOS
+#include <drm/sde_drm.h>
+#endif
+
 namespace sdm {
 
 /*! @brief This enum represents display layer blending types.
@@ -180,6 +184,10 @@ struct LayerFlags {
       uint32_t sde_preferred : 1;  //! This flag shall be set by client to indicate that this layer
                                    //! will be composed by display device, layer with this flag
                                    //! will have highest priority. To be used by OEMs only.
+#ifdef FOD_ZPOS
+      uint32_t fod_pressed : 1;    //!< This flag shall be set internally to mark the fod pressed
+                                   //!< layer
+#endif
     };
 
     uint32_t flags = 0;       //!< For initialization purpose only.
