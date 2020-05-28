9e40911f84db85195b593108cccd50b00b766b2b
Ramakant Singh <ramakant@codeaurora.org>
2020-05-28 16:23:51 +0530

    config: Disable Content Fps Detection
    Enable Fps Detection only for specific
    targets
    
    Change-Id: I259bf56b809c56f523b672cfe3e93b224d7b38ad

--- config/display-product.mk
+++ config/display-product.mk
@@ -125,7 +125,6 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.has_HDR_display=true
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.use_color_management=true
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.wcg_composition_dataspace=143261696
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.protected_contents=true
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.use_content_detection_for_refresh_rate=true
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.set_touch_timer_ms=200
 
 ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
