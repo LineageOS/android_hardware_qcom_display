LineageOS/android_device_motorola_nio

hwc: Combine all requests to stitch.
CRs-Fixed: 2660235
Change-Id: I752b69153ae8a4854e2be4f7324862c4444a5a9e

composer/gl_layer_stitch_impl.cpp
@@ -27,6 +27,8 @@
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#include <vector>
+
 #include "gl_layer_stitch_impl.h"
 
 #define __CLASS__ "GLLayerStitchImpl"
@@ -71,6 +73,10 @@ const char *kConvertRenderRGBShader = ""
   "{                                                                     \n"
   "    color = texture(u_sTexture, uv);                                  \n"
   "}                                                                     \n";
+
+static bool IsValid(const GLRect &rect) {
+  return ((rect.right - rect.left) && (rect.bottom - rect.top));
+}
 
 int GLLayerStitchImpl::CreateContext(bool secure) {
   ctx_.egl_display = eglGetDisplay(EGL_DEFAULT_DISPLAY);
@@ -128,41 +134,58 @@ int GLLayerStitchImpl::CreateContext(bool secure) {
   ctx_.program_id = LoadProgram(1, &kVertexShader1, count, fragment_shaders);
 
   SetRealTimePriority();
+  InitContext();
 
   return 0;
 }
 
-int GLLayerStitchImpl::Blit(const private_handle_t *src_hnd, const private_handle_t *dst_hnd,
-                            const GLRect &src_rect, const GLRect &dst_rect,
-                            const GLRect &scissor_rect, int src_acquire_fence_fd,
-                            int dst_acquire_fence_fd, int *release_fence_fd) {
+int GLLayerStitchImpl::Blit(const std::vector<StitchParams> &stitch_params, int *release_fence_fd) {
   DTRACE_SCOPED();
-  // eglMakeCurrent attaches rendering context to rendering surface.
-  MakeCurrent(&ctx_);
 
-  SetProgram(ctx_.program_id);
+  std::vector<int> acquire_fences;
+  std::vector<int> release_fences;
+  bool can_batch = !NeedsGLScissor(stitch_params);
+  for (auto &info : stitch_params) {
+    SetSourceBuffer(info.src_hnd);
+    SetDestinationBuffer(info.dst_hnd);
+    SetViewport(info.dst_rect);
+    ClearWithTransparency(info.scissor_rect);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
 
-  ClearWithTransparency(scissor_rect);
+    acquire_fences.push_back(info.src_acquire_fence_fd);
 
-  SetSourceBuffer(src_hnd);
-  SetDestinationBuffer(dst_hnd, dst_rect);
-
-  glEnableVertexAttribArray(0);
-  glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, kFullScreenVertices);
-  glEnableVertexAttribArray(1);
-  glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, kFullScreenTexCoords);
-  glDrawArrays(GL_TRIANGLES, 0, 3);
-
-  // Dst. is always guaranteed to be signaled.
-  int in_fence_fd = dup(src_acquire_fence_fd);
-  if (in_fence_fd >= 0) {
-    WaitOnInputFence(in_fence_fd);
+    if (!can_batch) {
+      // Trigger flush and cache release fence.
+      WaitOnInputFence(acquire_fences);
+      release_fences.push_back(CreateOutputFence());
+      acquire_fences = {};
+    }
   }
 
-  // Create output fence for client to wait on.
-  *release_fence_fd = CreateOutputFence();
+  if (can_batch) {
+    // Create output fence for client to wait on.
+    WaitOnInputFence(acquire_fences);
+    *release_fence_fd = CreateOutputFence();
+  } else {
+    // Merge all fd's and return one.
+    *release_fence_fd = GetMergedFd(release_fences, __CLASS__, false);
+    // Close all intermediate fences.
+    for (auto &fence : release_fences) {
+      close(fence);
+    }
+  }
 
   return 0;
+}
+
+int GLLayerStitchImpl::NeedsGLScissor(const std::vector<StitchParams> &stitch_params) {
+  for (auto &info : stitch_params) {
+    if (IsValid(info.scissor_rect)) {
+      return true;
+    }
+  }
+
+  return false;
 }
 
 int GLLayerStitchImpl::Init() {
@@ -177,12 +200,31 @@ int GLLayerStitchImpl::Deinit() {
 }
 
 void GLLayerStitchImpl::ClearWithTransparency(const GLRect &scissor_rect) {
+  if (!IsValid(scissor_rect)) {
+    // Disable scissor.
+    GL(glDisable(GL_SCISSOR_TEST));
+    return;
+  }
+
   DTRACE_SCOPED();
-  GL(glScissor(scissor_rect.left, scissor_rect.top, scissor_rect.right - scissor_rect.left,
-               scissor_rect.bottom - scissor_rect.top));
+  // Enable scissor test.
   GL(glEnable(GL_SCISSOR_TEST));
   GL(glClearColor(0, 0, 0, 0));
   GL(glClear(GL_COLOR_BUFFER_BIT));
+  GL(glScissor(scissor_rect.left, scissor_rect.top, scissor_rect.right - scissor_rect.left,
+               scissor_rect.bottom - scissor_rect.top));
+}
+
+void GLLayerStitchImpl::InitContext() {
+  // eglMakeCurrent attaches rendering context to rendering surface.
+  MakeCurrent(&ctx_);
+  SetProgram(ctx_.program_id);
+  SetRealTimePriority();
+  // Set vertices.
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, kFullScreenVertices);
+  glEnableVertexAttribArray(1);
+  glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, kFullScreenTexCoords);
 }
 
 GLLayerStitchImpl::~GLLayerStitchImpl() {}

composer/hwc_display_builtin.cpp
@@ -287,35 +287,36 @@ HWC2::Error HWCDisplayBuiltIn::CommitStitchLayers() {
     return HWC2::Error::None;
   }
 
+  LayerStitchContext ctx = {};
   for (auto &layer : layer_stack_.layers) {
     LayerComposition &composition = layer->composition;
     if (composition != kCompositionStitch) {
       continue;
     }
 
-    LayerStitchContext ctx = {};
+    StitchParams params = {};
     // Stitch target doesn't have an input fence.
     // Render all layers at specified destination.
     LayerBuffer &input_buffer = layer->input_buffer;
-    ctx.src_hnd = reinterpret_cast<const private_handle_t *>(input_buffer.buffer_id);
+    params.src_hnd = reinterpret_cast<const private_handle_t *>(input_buffer.buffer_id);
     Layer *stitch_layer = stitch_target_->GetSDMLayer();
     LayerBuffer &output_buffer = stitch_layer->input_buffer;
-    ctx.dst_hnd = reinterpret_cast<const private_handle_t *>(output_buffer.buffer_id);
-    SetRect(layer->stitch_info.dst_rect, &ctx.dst_rect);
-    SetRect(layer->stitch_info.slice_rect, &ctx.scissor_rect);
-    ctx.src_acquire_fence_fd = input_buffer.acquire_fence_fd;
+    params.dst_hnd = reinterpret_cast<const private_handle_t *>(output_buffer.buffer_id);
+    SetRect(layer->stitch_info.dst_rect, &params.dst_rect);
+    SetRect(layer->stitch_info.slice_rect, &params.scissor_rect);
+    params.src_acquire_fence_fd = input_buffer.acquire_fence_fd;
 
-    layer_stitch_task_.PerformTask(LayerStitchTaskCode::kCodeStitch, &ctx);
-
-    // Merge with current fence and close previous one.
-    int acquire_fence = -1;
-    buffer_sync_handler_.SyncMerge(output_buffer.acquire_fence_fd, ctx.release_fence_fd,
-                                   &acquire_fence);
-    CloseFd(&output_buffer.acquire_fence_fd);
-    CloseFd(&ctx.release_fence_fd);
-
-    output_buffer.acquire_fence_fd = acquire_fence;
+    ctx.stitch_params.push_back(params);
   }
+
+  if (!ctx.stitch_params.size()) {
+    // No layers marked for stitch.
+    return HWC2::Error::None;
+  }
+
+  layer_stitch_task_.PerformTask(LayerStitchTaskCode::kCodeStitch, &ctx);
+  // Set release fence.
+  output_buffer_.acquire_fence_fd = ctx.release_fence_fd;
 
   return HWC2::Error::None;
 }
@@ -1277,9 +1278,7 @@ void HWCDisplayBuiltIn::OnTask(const LayerStitchTaskCode &task_code,
     case LayerStitchTaskCode::kCodeStitch: {
         DTRACE_SCOPED();
         LayerStitchContext* ctx = reinterpret_cast<LayerStitchContext*>(task_context);
-        gl_layer_stitch_->Blit(ctx->src_hnd, ctx->dst_hnd, ctx->src_rect, ctx->dst_rect,
-                               ctx->scissor_rect, ctx->src_acquire_fence_fd,
-                               ctx->dst_acquire_fence_fd, &(ctx->release_fence_fd));
+        gl_layer_stitch_->Blit(ctx->stitch_params, &(ctx->release_fence_fd));
       }
       break;
     case LayerStitchTaskCode::kCodeDestroyInstance: {

LineageOS/android_hardware_qcom_display

composer/gl_layer_stitch_impl.cpp
@@ -27,6 +27,8 @@
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#include <vector>
+
 #include "gl_layer_stitch_impl.h"
 
 #define __CLASS__ "GLLayerStitchImpl"
@@ -71,6 +73,10 @@ const char *kConvertRenderRGBShader = ""
   "{                                                                     \n"
   "    color = texture(u_sTexture, uv);                                  \n"
   "}                                                                     \n";
+
+static bool IsValid(const GLRect &rect) {
+  return ((rect.right - rect.left) && (rect.bottom - rect.top));
+}
 
 int GLLayerStitchImpl::CreateContext(bool secure) {
   ctx_.egl_display = eglGetDisplay(EGL_DEFAULT_DISPLAY);
@@ -128,40 +134,57 @@ int GLLayerStitchImpl::CreateContext(bool secure) {
   ctx_.program_id = LoadProgram(1, &kVertexShader1, count, fragment_shaders);
 
   SetRealTimePriority();
+  InitContext();
 
   return 0;
 }
 
-int GLLayerStitchImpl::Blit(const private_handle_t *src_hnd, const private_handle_t *dst_hnd,
-                            const GLRect &src_rect, const GLRect &dst_rect,
-                            const GLRect &scissor_rect,
-                            const shared_ptr<Fence> &src_acquire_fence,
-                            const shared_ptr<Fence> &dst_acquire_fence,
+int GLLayerStitchImpl::Blit(const std::vector<StitchParams> &stitch_params,
                             shared_ptr<Fence> *release_fence) {
   DTRACE_SCOPED();
-  // eglMakeCurrent attaches rendering context to rendering surface.
-  MakeCurrent(&ctx_);
 
-  SetProgram(ctx_.program_id);
+  std::vector<shared_ptr<Fence>> acquire_fences;
+  std::vector<shared_ptr<Fence>> release_fences;
+  bool can_batch = !NeedsGLScissor(stitch_params);
+  for (auto &info : stitch_params) {
+    SetSourceBuffer(info.src_hnd);
+    SetDestinationBuffer(info.dst_hnd);
+    SetViewport(info.dst_rect);
+    ClearWithTransparency(info.scissor_rect);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
 
-  ClearWithTransparency(scissor_rect);
+    acquire_fences.push_back(info.src_acquire_fence);
 
-  SetSourceBuffer(src_hnd);
-  SetDestinationBuffer(dst_hnd, dst_rect);
+    if (!can_batch) {
+      // Trigger flush and cache release fence.
+      WaitOnInputFence(acquire_fences);
+      shared_ptr<Fence> temp_release_fence = nullptr;
+      CreateOutputFence(&temp_release_fence);
+      release_fences.push_back(temp_release_fence);
+      acquire_fences = {};
+    }
+  }
 
-  glEnableVertexAttribArray(0);
-  glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, kFullScreenVertices);
-  glEnableVertexAttribArray(1);
-  glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, kFullScreenTexCoords);
-  glDrawArrays(GL_TRIANGLES, 0, 3);
-
-  // Dst. is always guaranteed to be signaled.
-  WaitOnInputFence(src_acquire_fence);
-
-  // Create output fence for client to wait on.
-  CreateOutputFence(release_fence);
+  if (can_batch) {
+    // Create output fence for client to wait on.
+    WaitOnInputFence(acquire_fences);
+    CreateOutputFence(release_fence);
+  } else {
+    // Merge all fd's and return one.
+    *release_fence = Fence::Merge(release_fences, false);
+  }
 
   return 0;
+}
+
+int GLLayerStitchImpl::NeedsGLScissor(const std::vector<StitchParams> &stitch_params) {
+  for (auto &info : stitch_params) {
+    if (IsValid(info.scissor_rect)) {
+      return true;
+    }
+  }
+
+  return false;
 }
 
 int GLLayerStitchImpl::Init() {
@@ -176,12 +199,31 @@ int GLLayerStitchImpl::Deinit() {
 }
 
 void GLLayerStitchImpl::ClearWithTransparency(const GLRect &scissor_rect) {
+  if (!IsValid(scissor_rect)) {
+    // Disable scissor.
+    GL(glDisable(GL_SCISSOR_TEST));
+    return;
+  }
+
   DTRACE_SCOPED();
-  GL(glScissor(scissor_rect.left, scissor_rect.top, scissor_rect.right - scissor_rect.left,
-               scissor_rect.bottom - scissor_rect.top));
+  // Enable scissor test.
   GL(glEnable(GL_SCISSOR_TEST));
   GL(glClearColor(0, 0, 0, 0));
   GL(glClear(GL_COLOR_BUFFER_BIT));
+  GL(glScissor(scissor_rect.left, scissor_rect.top, scissor_rect.right - scissor_rect.left,
+               scissor_rect.bottom - scissor_rect.top));
+}
+
+void GLLayerStitchImpl::InitContext() {
+  // eglMakeCurrent attaches rendering context to rendering surface.
+  MakeCurrent(&ctx_);
+  SetProgram(ctx_.program_id);
+  SetRealTimePriority();
+  // Set vertices.
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, kFullScreenVertices);
+  glEnableVertexAttribArray(1);
+  glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, kFullScreenTexCoords);
 }
 
 GLLayerStitchImpl::~GLLayerStitchImpl() {}

composer/hwc_display_builtin.cpp
@@ -300,29 +300,36 @@ HWC2::Error HWCDisplayBuiltIn::CommitStitchLayers() {
     return HWC2::Error::None;
   }
 
+  LayerStitchContext ctx = {};
   for (auto &layer : layer_stack_.layers) {
     LayerComposition &composition = layer->composition;
     if (composition != kCompositionStitch) {
       continue;
     }
 
-    LayerStitchContext ctx = {};
+    StitchParams params = {};
     // Stitch target doesn't have an input fence.
     // Render all layers at specified destination.
     LayerBuffer &input_buffer = layer->input_buffer;
-    ctx.src_hnd = reinterpret_cast<const private_handle_t *>(input_buffer.buffer_id);
+    params.src_hnd = reinterpret_cast<const private_handle_t *>(input_buffer.buffer_id);
     Layer *stitch_layer = stitch_target_->GetSDMLayer();
     LayerBuffer &output_buffer = stitch_layer->input_buffer;
-    ctx.dst_hnd = reinterpret_cast<const private_handle_t *>(output_buffer.buffer_id);
-    SetRect(layer->stitch_info.dst_rect, &ctx.dst_rect);
-    SetRect(layer->stitch_info.slice_rect, &ctx.scissor_rect);
-    ctx.src_acquire_fence = input_buffer.acquire_fence;
+    params.dst_hnd = reinterpret_cast<const private_handle_t *>(output_buffer.buffer_id);
+    SetRect(layer->stitch_info.dst_rect, &params.dst_rect);
+    SetRect(layer->stitch_info.slice_rect, &params.scissor_rect);
+    params.src_acquire_fence = input_buffer.acquire_fence;
 
-    layer_stitch_task_.PerformTask(LayerStitchTaskCode::kCodeStitch, &ctx);
-
-    // Merge with current fence.
-    output_buffer.acquire_fence = Fence::Merge(output_buffer.acquire_fence, ctx.release_fence);
+    ctx.stitch_params.push_back(params);
   }
+
+  if (!ctx.stitch_params.size()) {
+    // No layers marked for stitch.
+    return HWC2::Error::None;
+  }
+
+  layer_stitch_task_.PerformTask(LayerStitchTaskCode::kCodeStitch, &ctx);
+  // Set release fence.
+  output_buffer_.acquire_fence = ctx.release_fence;
 
   return HWC2::Error::None;
 }
@@ -1306,9 +1313,7 @@ void HWCDisplayBuiltIn::OnTask(const LayerStitchTaskCode &task_code,
     case LayerStitchTaskCode::kCodeStitch: {
         DTRACE_SCOPED();
         LayerStitchContext* ctx = reinterpret_cast<LayerStitchContext*>(task_context);
-        gl_layer_stitch_->Blit(ctx->src_hnd, ctx->dst_hnd, ctx->src_rect, ctx->dst_rect,
-                               ctx->scissor_rect, ctx->src_acquire_fence,
-                               ctx->dst_acquire_fence, &(ctx->release_fence));
+        gl_layer_stitch_->Blit(ctx->stitch_params, &(ctx->release_fence));
       }
       break;
     case LayerStitchTaskCode::kCodeDestroyInstance: {