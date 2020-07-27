c07e3c8f88bdd2c80a9f88f623e4e21e4cf2478c
Dhruv Kaushik <dhrukaus@codeaurora.org>
2020-07-27 16:15:06 +0530

    displayconfig: Add API to query supported refresh rates from HAL
    Add HIDL API to query supported refresh rates from Display HAL.
    The API would return a vector of supported fps in the current
    config group.
    
    Change-Id: I27640114963a97c96738387fe502b802a48837ba
    CRs-Fixed: 2749264

--- services/config/src/device_impl.cpp
+++ services/config/src/device_impl.cpp
@@ -736,6 +736,25 @@ void DeviceImpl::DeviceClientContext::ParseGetDisplayHwId(const ByteStream &inpu
   _hidl_cb(error, output_params, {});
 }
 
+void DeviceImpl::DeviceClientContext::ParseGetSupportedDisplayRefreshRates(
+    const ByteStream &input_params, perform_cb _hidl_cb) {
+  ByteStream output_params;
+  std::vector<uint32_t> refresh_rates;
+
+  const uint8_t *data = input_params.data();
+  const DisplayType *dpy = reinterpret_cast<const DisplayType *>(data);
+  int32_t error = intf_->GetSupportedDisplayRefreshRates(*dpy, &refresh_rates);
+
+  uint32_t *refresh_rates_data =
+      reinterpret_cast<uint32_t *>(malloc(sizeof(uint32_t) * refresh_rates.size()));
+  for (int i = 0; i < refresh_rates.size(); i++) {
+    refresh_rates_data[i] = refresh_rates[i];
+  }
+  output_params.setToExternal(reinterpret_cast<uint8_t *>(refresh_rates_data),
+                              sizeof(uint32_t) * refresh_rates.size());
+  _hidl_cb(error, output_params, {});
+}
+
 Return<void> DeviceImpl::perform(uint64_t client_handle, uint32_t op_code,
                                  const ByteStream &input_params, const HandleStream &input_handles,
                                  perform_cb _hidl_cb) {
@@ -888,6 +907,9 @@ Return<void> DeviceImpl::perform(uint64_t client_handle, uint32_t op_code,
       break;
     case kGetDisplayHwId:
       client->ParseGetDisplayHwId(input_params, _hidl_cb);
+      break;
+    case kGetSupportedDisplayRefreshRates:
+      client->ParseGetSupportedDisplayRefreshRates(input_params, _hidl_cb);
       break;
     default:
       break;
