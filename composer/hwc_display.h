8bab03b8501a92a184d75ec21c67dbd1b18cdf28
Dileep Marchya <dmarchya@codeaurora.org>
2020-08-10 12:24:20 +0530

    Revert "utils: Add Fstat check in Fence"
    This reverts commit 99a18c13c3a5e1f5f87f416f97004c913e94f531.
    
    Change-Id: I13c98b9be314f89c7df937cbd9e122930fad87ad

--- composer/hwc_display.h
+++ composer/hwc_display.h
@@ -170,10 +170,7 @@ class HWCDisplay : public DisplayEventHandler {
     std::multiset<HWCLayer *, SortLayersByZ> layer_set;  // Maintain a set sorted by Z
   };
 
-  virtual ~HWCDisplay() {
-    int ret = 0;
-    ret = Fence::CheckFstat(layer_stack_.retire_fence);
-  }
+  virtual ~HWCDisplay() {}
   virtual int Init();
   virtual int Deinit();
 
--- sdm/libs/utils/fence.cpp
+++ sdm/libs/utils/fence.cpp
@@ -27,11 +27,7 @@
 * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
 #include <utils/fence.h>
-#include <utils/debug.h>
 #include <debug_handler.h>
 #include <assert.h>
 #include <string>
@@ -150,30 +146,6 @@ void Fence::Dump(std::ostringstream *os) {
   }
   */
   *os << "\n---------------------------------------\n";
-}
-
-int Fence::CheckFstat(const shared_ptr<Fence> &fence) {
-  struct stat buf1;
-  int ret;
-
-  if (!fence) {
-    DLOGW("Fence shared_ptr is NULL");
-    // Return 0 because only NULL on first cycle.
-    return 0;
-  }
-  if (!fence.get()) {
-    DLOGW("Underlying pointer is NULL.");
-    return 0;
-  }
-  ret = fstat(fence->fd_, &buf1);
-  if (ret) {
-    DLOGW("Fstat lookup failed");
-    return -1;
-  }
-  DLOGV("Fence fd=%d, ion=%llu, use_count=%d", fence->fd_, (uint64_t)buf1.st_ino,
-    fence.use_count());
-
-  return 0;
 }
 
 Fence::ScopedRef::~ScopedRef() {
