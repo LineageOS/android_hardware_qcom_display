497380bc756cb106eadaf1cd0524a8e0fda0c681
Demon Singur <demonsingur@gmail.com>
2020-08-23 13:21:47 +0200

    sdm: get around client compositioning for FOD pressed layer
    [Skyblueborb] Adapt for sm8150
    
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
    Change-Id: I51a1a447003076d7bf50d526355cbce98660052b

--- sdm/libs/core/drm/hw_device_drm.cpp
+++ sdm/libs/core/drm/hw_device_drm.cpp
@@ -1177,7 +1177,9 @@ void HWDeviceDRM::SetupAtomic(HWLayers *hw_layers, bool validate) {
 
 #ifdef FOD_ZPOS
           uint32_t z_order = pipe_info->z_order;
-          if (layer.flags.fod_pressed) {
+          if (layer.flags.fod_pressed
+              || (hw_layer_info.stack->flags.fod_pressed_present
+                && i == hw_layer_count - 1)) {
             z_order |= FOD_PRESSED_LAYER_ZORDER;
           }
           drm_atomic_intf_->Perform(DRMOps::PLANE_SET_ZORDER, pipe_id, z_order);
