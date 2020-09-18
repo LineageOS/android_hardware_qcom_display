41b61234996c1fa5ff22cdc39a8615d32fec8f5e
Arne Coucheron <arco68@gmail.com>
2020-09-18 04:35:17 +0200

    Revert "Don't force GPU composite for older devices"
    There's an issue on devices with MDP rev. 4.3 and lower,
    with random black/white screen causing the system to freeze
    for a while. Reproducible with Chrome based browsers.
    
    Debugging traced the issue back here, and testing shows that
    this workaround is likely not needed anymore.
    
    Reverting it makes Chrome work again without crashing, and
    Netflix still works as it should.
    
    This reverts commit 5b934669ca89a391fddf7da2e80613025114ca67.
    
    Change-Id: I7f969e87de29b630e2be04bf4876c48cd3051bc7

--- libhwcomposer/hwc_mdpcomp.cpp
+++ libhwcomposer/hwc_mdpcomp.cpp
@@ -464,7 +464,7 @@ bool MDPComp::isFullFrameDoable(hwc_context_t *ctx,
         }
 
         // If buffer is non contiguous then force GPU comp
-        if(isNonContigBuffer(hnd) && ctx->mMDP.version > qdutils::MDP_V4_3) {
+        if(isNonContigBuffer(hnd)) {
             ALOGD_IF(isDebug(), "%s: Buffer is Non contiguous,"
                                 "so mdpcomp is not possible",__FUNCTION__);
             return false;
@@ -636,7 +636,7 @@ bool MDPComp::isOnlyVideoDoable(hwc_context_t *ctx,
         }
         private_handle_t *hnd = (private_handle_t *)layer->handle;
         // If buffer is non contiguous then force GPU comp
-        if(isNonContigBuffer(hnd) && ctx->mMDP.version > qdutils::MDP_V4_3) {
+        if(isNonContigBuffer(hnd)) {
             ALOGD_IF(isDebug(), "%s: Buffer is Non contiguous,"
                                 "so mdpcomp is not possible",__FUNCTION__);
             return false;
