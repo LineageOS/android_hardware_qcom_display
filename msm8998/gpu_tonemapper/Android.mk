47d6419815bf3899bc0bf952781c519213c74276
Pig <pig.priv@gmail.com>
2020-09-19 09:19:29 +0800

    display: Drop LOCAL_COPY_HEADERS usage
    LOCAL_COPY_HEADERS is deprecated, so remove all its usages
    to avoid build warnings.
    
    Change-Id: I964a11f036e03fb9fa2dec05a1698c118b06b421

--- msm8998/gpu_tonemapper/Android.mk
+++ msm8998/gpu_tonemapper/Android.mk
@@ -2,19 +2,12 @@ LOCAL_PATH := $(call my-dir)
 include $(LOCAL_PATH)/../common.mk
 
 include $(CLEAR_VARS)
-LOCAL_VENDOR_MODULE       := true
-LOCAL_COPY_HEADERS_TO     := $(common_header_export_path)
-LOCAL_COPY_HEADERS        := TonemapFactory.h Tonemapper.h
-LOCAL_VENDOR_MODULE       := true
-include $(BUILD_COPY_HEADERS)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE              := libgpu_tonemapper
 LOCAL_LICENSE_KINDS       := SPDX-license-identifier-Apache-2.0 legacy_not_a_contribution
 LOCAL_LICENSE_CONDITIONS  := by_exception_only not_allowed notice
 LOCAL_VENDOR_MODULE       := true
 LOCAL_MODULE_TAGS         := optional
-LOCAL_C_INCLUDES          := $(TARGET_OUT_HEADERS)/qcom/display/
+LOCAL_HEADER_LIBRARIES    := display_headers
 LOCAL_C_INCLUDES          += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 LOCAL_SHARED_LIBRARIES    := libEGL libGLESv2 libGLESv3 libui libutils liblog
 
