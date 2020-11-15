5555acb92f51a0e78c37846906752b870eecd208
Arne Coucheron <arco68@gmail.com>
2020-11-15 01:15:08 +0100

    msm8974: Add display headers lib
    LOCAL_COPY_HEADERS is deprecated. Replace with
    headers lib to avoid build warnings.
    
    Change-Id: I764fd3f37f9a43a921a76c3775664de39594f721

--- msm8974/libhwcomposer/Android.mk
+++ msm8974/libhwcomposer/Android.mk
@@ -8,7 +8,6 @@ LOCAL_MODULE                  := hwcomposer.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_RELATIVE_PATH    := hw
 LOCAL_PROPRIETARY_MODULE      := true
 LOCAL_MODULE_TAGS             := optional
-LOCAL_C_INCLUDES              := $(common_includes)
 LOCAL_SHARED_LIBRARIES        := $(common_libs) libEGL liboverlay \
                                  libexternal libqdutils libhardware_legacy \
                                  libdl libmemalloc libqservice libsync \
@@ -16,7 +15,7 @@ LOCAL_SHARED_LIBRARIES        := $(common_libs) libEGL liboverlay \
                                  libbfqio
 
 LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"qdhwcomposer\"
-LOCAL_HEADER_LIBRARIES        := generated_kernel_headers
+LOCAL_HEADER_LIBRARIES        := display_headers generated_kernel_headers
 LOCAL_SRC_FILES               := hwc.cpp          \
                                  hwc_utils.cpp    \
                                  hwc_uevents.cpp  \
--- msm8974/libqservice/Android.mk
+++ msm8974/libqservice/Android.mk
@@ -5,17 +5,13 @@ include $(CLEAR_VARS)
 LOCAL_MODULE                  := libqservice
 LOCAL_MODULE_PATH             := $(TARGET_OUT_SHARED_LIBRARIES)
 LOCAL_MODULE_TAGS             := optional
-LOCAL_C_INCLUDES              := $(common_includes)
 LOCAL_SHARED_LIBRARIES        := $(common_libs) libexternal libbinder
 LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"qdqservice\"
 LOCAL_CFLAGS                  += -Wno-error
-LOCAL_HEADER_LIBRARIES        := generated_kernel_headers
+LOCAL_HEADER_LIBRARIES        := display_headers generated_kernel_headers
 LOCAL_SRC_FILES               := QService.cpp \
                                  IQService.cpp \
                                  IQClient.cpp
-LOCAL_COPY_HEADERS_TO         := $(common_header_export_path)
-LOCAL_COPY_HEADERS            := IQService.h \
-                                 IQClient.h
 
 
 include $(BUILD_SHARED_LIBRARY)
