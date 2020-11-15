a6b4e57e247eb6d8b5f04c367c6e29aade7afd4d
Arne Coucheron <arco68@gmail.com>
2020-11-15 01:15:08 +0100

    msm8084: Add display headers lib
    LOCAL_COPY_HEADERS is deprecated. Replace with
    headers lib to avoid build warnings.
    
    Change-Id: I5512cec25ad8f93cbdf97ab817e608e8628d45ff

--- msm8084/libhwcomposer/Android.mk
+++ msm8084/libhwcomposer/Android.mk
@@ -6,9 +6,6 @@ LOCAL_MODULE                  := hwcomposer.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_RELATIVE_PATH    := hw
 LOCAL_PROPRIETARY_MODULE      := true
 LOCAL_MODULE_TAGS             := optional
-LOCAL_C_INCLUDES              := $(common_includes) \
-                                 $(TOP)/external/skia/include/core \
-                                 $(TOP)/external/skia/include/images
 LOCAL_SHARED_LIBRARIES        := $(common_libs) libEGL liboverlay \
                                  libexternal libqdutils libhardware_legacy \
                                  libdl libmemalloc libqservice libsync \
@@ -20,7 +17,7 @@ LOCAL_SHARED_LIBRARIES += libhwui
 endif #TARGET_USES_QCOM_BSP
 
 LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"qdhwcomposer\"
-LOCAL_HEADER_LIBRARIES        := generated_kernel_headers
+LOCAL_HEADER_LIBRARIES        := display_headers generated_kernel_headers
 LOCAL_SRC_FILES               := hwc.cpp          \
                                  hwc_utils.cpp    \
                                  hwc_uevents.cpp  \
