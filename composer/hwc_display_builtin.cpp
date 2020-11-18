codeaurora-unoffical/platform-hardware-qcom-display

hwc: Set Framebuffer target always.
-- EGLImages are cached as part of LRU cache.
-- Avoiding access to a particular image can lead
   to deletion of EGLImage.
-- Fix populating blit fence.

CRs-Fixed: 2708188
Change-Id: I9c61bb566a7e194a1fcd5c4dc2f085645a3bf730

composer/hwc_display_builtin.cpp
@@ -295,6 +295,8 @@ HWC2::Error HWCDisplayBuiltIn::CommitStitchLayers() {
   }
 
   LayerStitchContext ctx = {};
+  Layer *stitch_layer = stitch_target_->GetSDMLayer();
+  LayerBuffer &output_buffer = stitch_layer->input_buffer;
   for (auto &layer : layer_stack_.layers) {
     LayerComposition &composition = layer->composition;
     if (composition != kCompositionStitch) {
@@ -306,8 +308,6 @@ HWC2::Error HWCDisplayBuiltIn::CommitStitchLayers() {
     // Render all layers at specified destination.
     LayerBuffer &input_buffer = layer->input_buffer;
     params.src_hnd = reinterpret_cast<const private_handle_t *>(input_buffer.buffer_id);
-    Layer *stitch_layer = stitch_target_->GetSDMLayer();
-    LayerBuffer &output_buffer = stitch_layer->input_buffer;
     params.dst_hnd = reinterpret_cast<const private_handle_t *>(output_buffer.buffer_id);
     SetRect(layer->stitch_info.dst_rect, &params.dst_rect);
     SetRect(layer->stitch_info.slice_rect, &params.scissor_rect);
@@ -323,7 +323,7 @@ HWC2::Error HWCDisplayBuiltIn::CommitStitchLayers() {
 
   layer_stitch_task_.PerformTask(LayerStitchTaskCode::kCodeStitch, &ctx);
   // Set release fence.
-  output_buffer_.acquire_fence_fd = ctx.release_fence_fd;
+  output_buffer.acquire_fence_fd = ctx.release_fence_fd;
 
   return HWC2::Error::None;
 }

LineageOS/android_hardware_qcom_display

composer/hwc_display_builtin.cpp
@@ -347,6 +347,8 @@ HWC2::Error HWCDisplayBuiltIn::CommitStitchLayers() {
   }
 
   LayerStitchContext ctx = {};
+  Layer *stitch_layer = stitch_target_->GetSDMLayer();
+  LayerBuffer &output_buffer = stitch_layer->input_buffer;
   for (auto &layer : layer_stack_.layers) {
     LayerComposition &composition = layer->composition;
     if (composition != kCompositionStitch) {
@@ -358,8 +360,6 @@ HWC2::Error HWCDisplayBuiltIn::CommitStitchLayers() {
     // Render all layers at specified destination.
     LayerBuffer &input_buffer = layer->input_buffer;
     params.src_hnd = reinterpret_cast<const private_handle_t *>(input_buffer.buffer_id);
-    Layer *stitch_layer = stitch_target_->GetSDMLayer();
-    LayerBuffer &output_buffer = stitch_layer->input_buffer;
     params.dst_hnd = reinterpret_cast<const private_handle_t *>(output_buffer.buffer_id);
     SetRect(layer->stitch_info.dst_rect, &params.dst_rect);
     SetRect(layer->stitch_info.slice_rect, &params.scissor_rect);
@@ -375,7 +375,7 @@ HWC2::Error HWCDisplayBuiltIn::CommitStitchLayers() {
 
   layer_stitch_task_.PerformTask(LayerStitchTaskCode::kCodeStitch, &ctx);
   // Set release fence.
-  output_buffer_.acquire_fence = ctx.release_fence;
+  output_buffer.acquire_fence = ctx.release_fence;
 
   return HWC2::Error::None;
 }