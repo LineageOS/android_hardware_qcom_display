46f107799e33998777d97201cc7b3192d4fe8a8a
Arne Coucheron <arco68@gmail.com>
2020-11-18 12:10:13 +0000

    display: Use generated kernel headers
    Change-Id: Ia1e22dc714f055c966c9ca297523ed33a8c92c8e

--- libqdutils/Android.mk
+++ libqdutils/Android.mk
@@ -6,9 +6,9 @@ LOCAL_MODULE                  := libqdutils
 LOCAL_MODULE_TAGS             := optional
 LOCAL_VENDOR_MODULE           := true
 LOCAL_SHARED_LIBRARIES        := $(common_libs) libui libbinder libqservice
-LOCAL_C_INCLUDES              := $(common_includes) $(kernel_includes)
+LOCAL_C_INCLUDES              := $(common_includes)
 LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"qdutils\" -Wno-float-conversion
-LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps)
+LOCAL_HEADER_LIBRARIES        := generated_kernel_headers
 LOCAL_COPY_HEADERS_TO         := $(common_header_export_path)
 LOCAL_COPY_HEADERS            := display_config.h mdp_version.h
 LOCAL_SRC_FILES               := profiler.cpp mdp_version.cpp \
@@ -24,7 +24,7 @@ LOCAL_COPY_HEADERS_TO           := $(common_header_export_path)
 LOCAL_COPY_HEADERS              := qdMetaData.h qd_utils.h
 LOCAL_SHARED_LIBRARIES          := liblog libcutils
 LOCAL_C_INCLUDES                := $(common_includes)
-LOCAL_ADDITIONAL_DEPENDENCIES   := $(common_deps)
+LOCAL_HEADER_LIBRARIES          := generated_kernel_headers
 LOCAL_SRC_FILES                 := qdMetaData.cpp qd_utils.cpp
 LOCAL_CFLAGS                    := $(common_flags)
 LOCAL_CFLAGS                    += -DLOG_TAG=\"DisplayMetaData\"
@@ -38,7 +38,7 @@ include $(CLEAR_VARS)
 
 LOCAL_SHARED_LIBRARIES          := liblog libcutils
 LOCAL_C_INCLUDES                := $(common_includes)
-LOCAL_ADDITIONAL_DEPENDENCIES   := $(common_deps)
+LOCAL_HEADER_LIBRARIES          := generated_kernel_headers
 LOCAL_SRC_FILES                 := qdMetaData.cpp qd_utils.cpp
 LOCAL_CFLAGS                    := $(common_flags) -Wno-sign-conversion
 LOCAL_CFLAGS                    += -DLOG_TAG=\"DisplayMetaData\"
