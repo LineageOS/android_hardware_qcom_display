bc7aebd29437fe40a59e5fce60472a79145b52d3
Arne Coucheron <arco68@gmail.com>
2020-11-18 12:55:26 +0000

    display: Add display headers lib
    LOCAL_COPY_HEADERS is deprecated. Replace with
    headers lib to avoid build warnings.
    
    Change-Id: I5512cec25ad8f93cbdf97ab817e608e8628d45ff

--- liboverlay/Android.mk
+++ liboverlay/Android.mk
@@ -5,11 +5,10 @@ include $(CLEAR_VARS)
 LOCAL_MODULE                  := liboverlay
 LOCAL_MODULE_TAGS             := optional
 LOCAL_VENDOR_MODULE           := true
-LOCAL_C_INCLUDES              := $(common_includes)
 LOCAL_SHARED_LIBRARIES        := $(common_libs) libqdutils libmemalloc \
                                  libsync libdl
 LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"qdoverlay\"
-LOCAL_HEADER_LIBRARIES        := generated_kernel_headers
+LOCAL_HEADER_LIBRARIES        := generated_kernel_headers display_headers
 LOCAL_SRC_FILES := \
       overlay.cpp \
       overlayUtils.cpp \
