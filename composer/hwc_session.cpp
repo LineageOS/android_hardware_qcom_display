ca778fb3e656feb1fc3c993f85bb07be00f52954
Ramakant Singh <ramakant@codeaurora.org>
2020-11-28 15:00:48 +0530

    composer: Fix async power mode issue
    After getting first power override call
    from sf enable async power mode.
    
    Change-Id: I1adb90ac67b7dd916a5ac1ad7cc431d11e58f719

--- composer/hwc_session.cpp
+++ composer/hwc_session.cpp
@@ -1156,7 +1156,8 @@ int32_t HWCSession::SetPowerMode(hwc2_display_t display, int32_t int_mode) {
   }
 
   // async_powermode supported for power on and off
-  bool override_mode = async_powermode_ && display_ready_.test(UINT32(display));
+  bool override_mode = async_powermode_ && display_ready_.test(UINT32(display)) &&
+                       async_power_mode_triggered_;
   HWC2::PowerMode last_power_mode = hwc_display_[display]->GetCurrentPowerMode();
 
   if (last_power_mode == mode) {
