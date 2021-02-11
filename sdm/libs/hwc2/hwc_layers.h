6d0b569576730bfe87c99da029042c2370db7047
Pig <pig.priv@gmail.com>
2021-02-11 10:45:21 +0800

    Revert "sdm: mark FOD pressed layer by setting a bit on ZPOS"
    This reverts commit 2d33ccf676b2bcebc31e8b11c5b38f17b6de7942.
    
    Change-Id: I6faba624662cab99eb4b7ca55a73bb3a9268c5a9

--- sdm/libs/hwc2/hwc_layers.h
+++ sdm/libs/hwc2/hwc_layers.h
@@ -119,9 +119,6 @@ class HWCLayer {
   void SetLayerAsMask();
   bool BufferLatched() { return buffer_flipped_; }
   void ResetBufferFlip() { buffer_flipped_ = false; }
-#ifdef FOD_ZPOS
-  bool IsFodPressed() { return fod_pressed_; }
-#endif
 
  private:
   Layer *layer_ = nullptr;
@@ -142,9 +139,6 @@ class HWCLayer {
   bool non_integral_source_crop_ = false;
   bool has_metadata_refresh_rate_ = false;
   bool buffer_flipped_ = false;
-#ifdef FOD_ZPOS
-  bool fod_pressed_ = false;
-#endif
 
   // Composition requested by client(SF) Original
   HWC2::Composition client_requested_orig_ = HWC2::Composition::Device;
