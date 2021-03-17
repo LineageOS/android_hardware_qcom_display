be4280ba69de641e9c0725e99d897c0a5c09d149
Rheygine Medel <rmedel@codeaurora.org>
2021-03-16 21:52:57 -0700

    composer: Use private_handle_t for CWB
    This change uses private_handle_t to allow CWB to retrieve the
    buffer's metadata directly through the handle.
    
    Change-Id: I4daa2e477995a0a965ebd4f5b0d44dd2f1628c2d
    CRs-Fixed: 2901988

--- composer/hwc_display_builtin.cpp
+++ composer/hwc_display_builtin.cpp
@@ -640,40 +640,28 @@ HWC2::Error HWCDisplayBuiltIn::SetReadbackBuffer(const native_handle_t *buffer,
     return HWC2::Error::Unsupported;
   }
 
-  const native_handle_t *handle = reinterpret_cast<const native_handle_t *>(buffer);
-  int fd;
-  buffer_allocator_->GetFd((void *)handle, fd);
-  uint32_t width, height, unaligned_width, unaligned_height = 0;
-  int32_t format, flags = 0;
-  uint64_t id = 0;
-  buffer_allocator_->GetWidth((void *)handle, width);
-  buffer_allocator_->GetHeight((void *)handle, height);
-  buffer_allocator_->GetUnalignedWidth((void *)handle, unaligned_width);
-  buffer_allocator_->GetUnalignedHeight((void *)handle, unaligned_height);
-  buffer_allocator_->GetFormat((void *)handle, format);
-  buffer_allocator_->GetPrivateFlags((void *)handle, flags);
-  buffer_allocator_->GetBufferId((void *)handle, id);
+  const private_handle_t *handle = reinterpret_cast<const private_handle_t *>(buffer);
 
   if (!handle) {
     DLOGE("Bad parameter: handle is null");
     return HWC2::Error::BadParameter;
   }
 
-  if (fd < 0) {
+  if (handle->fd < 0) {
     DLOGE("Bad parameter: fd is null");
     return HWC2::Error::BadParameter;
   }
 
   // Configure the output buffer as Readback buffer
-  output_buffer_.width = UINT32(width);
-  output_buffer_.height = UINT32(height);
-  output_buffer_.unaligned_width = UINT32(unaligned_width);
-  output_buffer_.unaligned_height = UINT32(unaligned_height);
-  output_buffer_.format = HWCLayer::GetSDMFormat(format, flags);
-  output_buffer_.planes[0].fd = fd;
-  output_buffer_.planes[0].stride = UINT32(width);
+  output_buffer_.width = UINT32(handle->width);
+  output_buffer_.height = UINT32(handle->height);
+  output_buffer_.unaligned_width = UINT32(handle->unaligned_width);
+  output_buffer_.unaligned_height = UINT32(handle->unaligned_height);
+  output_buffer_.format = HWCLayer::GetSDMFormat(handle->format, handle->flags);
+  output_buffer_.planes[0].fd = handle->fd;
+  output_buffer_.planes[0].stride = UINT32(handle->width);
   output_buffer_.acquire_fence = acquire_fence;
-  output_buffer_.handle_id = id;
+  output_buffer_.handle_id = handle->id;
 
   post_processed_output_ = post_processed_output;
   readback_buffer_queued_ = true;
