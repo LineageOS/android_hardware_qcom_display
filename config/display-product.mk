0caef400eaede4c84bb799a47a07c7c61bc51f0a
Rajat Yadav <rajayada@codeaurora.org>
2021-06-17 15:13:51 +0530

    display-product.mk : Adding Properties for Khaje.
    Change-Id: Ic296af92335d39a2d52958ce17eb2123104940fd

--- config/display-product.mk
+++ config/display-product.mk
@@ -79,8 +79,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.display.enable_camera_smooth=1 \
     vendor.display.enable_allow_idle_fallback=1
 
-# Enable offline rotator for Bengal, Monaco.
-ifneq ($(filter bengal monaco, $(TARGET_BOARD_PLATFORM)),$(TARGET_BOARD_PLATFORM))
+# Enable offline rotator for Bengal, Monaco, Khaje.
+ifneq ($(filter bengal monaco khaje, $(TARGET_BOARD_PLATFORM)),$(TARGET_BOARD_PLATFORM))
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.display.disable_offline_rotator=1
 else
