380ae96e7177ad76698ab294178e68472d3a102a
Rajat Yadav <rajayada@codeaurora.org>
2021-06-21 16:17:42 +0530

    composer: Initialize GLColorConvert to nullptr.
    CRs-Fixed: 2971382
    Change-Id: I80bb5b4f66bbc365569f5a6d9b180c4943e832df

--- composer/hwc_display_virtual_gpu.h
+++ composer/hwc_display_virtual_gpu.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019-2020, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2019-2021, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -76,7 +76,7 @@ class HWCDisplayVirtualGPU : public HWCDisplayVirtual,
               SyncTask<ColorConvertTaskCode>::TaskContext *task_context);
 
   SyncTask<ColorConvertTaskCode> color_convert_task_;
-  GLColorConvert *gl_color_convert_;
+  GLColorConvert *gl_color_convert_ = nullptr;
 
   bool disable_animation_ = false;
   bool animation_in_progress_ = false;
