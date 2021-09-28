67051221cbc66820d431923f18ec6509827d1c8e
Akshay Ashtunkar <quic_akshayaa@quicinc.com>
2021-09-28 16:26:06 +0530

    sde-drm: Fix vector capacity issue for LTM on dual displays
    Ltm buffer ctrl map has the less capacity than the display count,
    which leads to reallocation. Due to reallocation the initial pointer
    which is present in the dpps_dirty_prop pointing to the old address.
    To fix the issue in Init, reserve the size for the max display count.
    
    Change-Id: Ic55706d2bd26c41bb8250225e192f1bab4f39233

--- sde-drm/drm_dpps_mgr_imp.cpp
+++ sde-drm/drm_dpps_mgr_imp.cpp
@@ -1,5 +1,5 @@
 /*
-* Copyright (c) 2019-2020, The Linux Foundation. All rights reserved.
+* Copyright (c) 2019-2021, The Linux Foundation. All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without
 * modification, are permitted provided that the following conditions are
@@ -313,6 +313,9 @@ void DRMDppsManagerImp::Init(int fd, drmModeRes* res) {
   dpps_feature_[kFeatureLtmHistEvent] = DRMDppsPropInfo{1, DRMProperty::INVALID, 0, true /* is_event */};
   dpps_feature_[kFeatureLtmWbPbEvent] = DRMDppsPropInfo{1, DRMProperty::INVALID, 0, true /* is_event */};
   dpps_feature_[kFeatureLtmOffEvent] = DRMDppsPropInfo{1, DRMProperty::INVALID, 0, true /* is_event */};
+
+  ltm_buffers_ctrl_map_.reserve(MAX_DISPLAY_COUNT);
+  ltm_buffers_map_.reserve(MAX_DISPLAY_COUNT);
 }
 
 void DRMDppsManagerImp::CacheDppsFeature(uint32_t obj_id, va_list args) {
