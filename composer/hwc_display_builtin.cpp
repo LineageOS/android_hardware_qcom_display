c84ac83d9270faedcd19ee31afb41e79e4fce74a
Vikas batchu <quic_vikabatc@quicinc.com>
2022-01-17 17:14:18 +0530

    composer: add null pointer check for cpu hint
    Change-Id: Ic58ca93d4399de375c0dc94e21a430bd64e83f76

--- composer/hwc_display_builtin.cpp
+++ composer/hwc_display_builtin.cpp
@@ -1595,6 +1595,10 @@ HWC2::Error HWCDisplayBuiltIn::SetDimmingMinBl(int min_bl) {
 }
 
 void HWCDisplayBuiltIn::HandleLargeCompositionHint(bool release) {
+  if (!cpu_hint_) {
+    return;
+  }
+
   int tid = gettid();
 
   if (release) {
