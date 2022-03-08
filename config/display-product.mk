d5c72652460e5f161864a41cd543d6f9378270de
Akshay Ashtunkar <quic_akshayaa@quicinc.com>
2022-03-08 11:26:13 +0530

    display: Add CLSTC xml config file
    Change-Id: I61b1d146725b679fe49d38dbd6f950b089da3819
    CRs-Fixed: 3161367

--- config/display-product.mk
+++ config/display-product.mk
@@ -33,6 +33,9 @@ PRODUCT_COPY_FILES += hardware/qcom/display/config/qdcm_calib_data_bengal_defaul
 
 #Multi-stc libraries config xml file
 PRODUCT_COPY_FILES += hardware/qcom/display/config/snapdragon_color_libs_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/snapdragon_color_libs_config.xml
+
+#Clstc library config xml file
+PRODUCT_COPY_FILES += hardware/qcom/display/config/clstc_config_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/clstc_config_library.xml
 endif
 #QDCM calibration xml file for td4330 panel
 PRODUCT_COPY_FILES += hardware/qcom/display/config/qdcm_calib_data_bengal_default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_td4330_v2_cmd_mode_dsi_truly_panel.xml
