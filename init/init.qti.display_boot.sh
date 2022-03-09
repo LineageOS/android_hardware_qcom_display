d39e3bb4c4366f01a63cd1872954b70d10d56812
Rheygine Medel <quic_rmedel@quicinc.com>
2022-03-09 13:10:07 -0800

    init: Enable spec fence by default
    Change-Id: Idcb5bc53c547920715604fa3dbe7a64c923ec859
    CRs-Fixed: 3150591

--- init/init.qti.display_boot.sh
+++ init/init.qti.display_boot.sh
@@ -48,7 +48,7 @@ case "$target" in
         setprop vendor.display.enable_allow_idle_fallback 1
         setprop vendor.display.enable_perf_hint_large_comp_cycle 1
         setprop vendor.display.enable_rotator_ui 1
-        setprop vendor.display.enable_spec_fence 0
+        setprop vendor.display.enable_spec_fence 1
         setprop vendor.display.thermal.version 1
         setprop vendor.display.enable_rc_support 1
         ;;
@@ -67,7 +67,7 @@ case "$target" in
         setprop vendor.display.enable_allow_idle_fallback 1
         setprop vendor.display.enable_perf_hint_large_comp_cycle 1
         setprop vendor.display.enable_rotator_ui 1
-        setprop vendor.display.enable_spec_fence 0
+        setprop vendor.display.enable_spec_fence 1
         setprop vendor.display.thermal.version 1
         setprop vendor.display.enable_rc_support 1
         setprop vendor.display.target.version 3
