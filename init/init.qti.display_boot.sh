ca17b9dd728cd67308efeab9b7ff035baf46ac7f
Ajay Singh Parmar <quic_aparmar@quicinc.com>
2022-06-29 23:05:07 -0700

    Revert "Revert "init: Enable iwe by default""
    This reverts commit 1859a922e7b7e79f4f75b8bd6c4beef0563f9261.
    
    Change-Id: I1dbe6958b522d0dc3820fed30c01513875c8e7de

--- init/init.qti.display_boot.sh
+++ init/init.qti.display_boot.sh
@@ -52,6 +52,7 @@ case "$target" in
         setprop vendor.display.thermal.version 1
         setprop vendor.display.enable_rc_support 1
         setprop vendor.display.enable_latch_media_content 1
+        setprop vendor.display.enable_inline_writeback 1
         ;;
     esac
     ;;
