e64fe72529c09af83f8eeefa4d4014ab8edc644b
Rheygine Medel <quic_rmedel@quicinc.com>
2022-07-05 15:35:27 -0700

    composer: Control large composition hint with the property
    Composer tries to release the large composition hint and renew HWC's
    TID even when the vendor property is disabled. This change allows the
    property to fully enable or disable the hint.
    
    Change-Id: If8d023871e8c996eacb5f0a6ccdc01d4de84555f
    CRs-Fixed: 3231493

--- composer/hwc_display_builtin.cpp
+++ composer/hwc_display_builtin.cpp
@@ -1337,10 +1337,8 @@ int HWCDisplayBuiltIn::PostInit() {
 }
 
 bool HWCDisplayBuiltIn::NeedsLargeCompPerfHint() {
-  if (!cpu_hint_ || !perf_hint_large_comp_cycle_) {
-    DLOGV_IF(kTagResources, "cpu_hint_:%d not initialized or property:%d not set",
-             !cpu_hint_, !perf_hint_large_comp_cycle_);
-
+  if (!cpu_hint_) {
+    DLOGV_IF(kTagResources, "CPU hint is not initialized");
     return false;
   }
 
@@ -1492,8 +1490,12 @@ HWC2::Error HWCDisplayBuiltIn::CommitOrPrepare(bool validate_only,
 
   auto status = HWCDisplay::CommitOrPrepare(validate_only, out_retire_fence, out_num_types,
                                             out_num_requests, needs_commit);
-  bool needs_hint = NeedsLargeCompPerfHint();
-  HandleLargeCompositionHint(!needs_hint);
+
+  if (perf_hint_large_comp_cycle_) {
+    bool needs_hint = NeedsLargeCompPerfHint();
+    HandleLargeCompositionHint(!needs_hint);
+  }
+
   return status;
 }
 
