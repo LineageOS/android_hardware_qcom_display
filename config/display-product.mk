fcc43916381690ee9c763b66bab52f11a2081c6e
Pullakavi Srinivas <quic_spullaka@quicinc.com>
2022-07-21 22:45:30 +0530

    hwc: Disable SF composition prediction model.
    CRs-Fixed: 3243885
    Change-Id: I64f82a25768b561bb343fdb7686b7d6c020d024a

--- config/display-product.mk
+++ config/display-product.mk
@@ -83,7 +83,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.display.disable_sdr_dimming=1 \
     vendor.display.enable_rc_support=1 \
     vendor.display.disable_sdr_histogram=1 \
-    vendor.display.enable_hdr10_gpu_target=1
+    vendor.display.enable_hdr10_gpu_target=1 \
+    debug.sf.predict_hwc_composition_strategy=0
 
 # Enable offline rotator for Bengal.
 ifneq ($(TARGET_BOARD_PLATFORM),bengal)
