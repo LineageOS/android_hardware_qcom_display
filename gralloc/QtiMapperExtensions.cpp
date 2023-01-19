a6fb14b5760718615114eec528e69942d6c2faa4
Pranav Vashi <neobuddy89@gmail.com>
2023-01-19 18:11:50 +0530

    Revert "gralloc: Add support for CRC calculation"
    Qcom introduced these interfaces in LA.QSSI.11.0, but not in 12.0
    and 13.0, maybe because they are not needed for future chipsets.
    As a result, only lahaina needs those interfaces.
    
    Revert this commit such that it can be built with LA.QSSI.13.0, which
    are tags for commonsys display libs.
    
    Thanks Jake Weinstein for the information.
    
    This reverts commit 10d758df9d3ff2fab47d0f5dd1272f6cf6f7fbdd.
    
    Change-Id: I11542facc60376f0179fb70a71585a70c37bef82

--- gralloc/QtiMapperExtensions.cpp
+++ gralloc/QtiMapperExtensions.cpp
@@ -25,40 +25,8 @@
  * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
  * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- * Changes from Qualcomm Innovation Center are provided under the following license:
- * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted (subject to the limitations in the
- * disclaimer below) provided that the following conditions are met:
- *
- *     * Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *
- *     * Redistributions in binary form must reproduce the above
- *     copyright notice, this list of conditions and the following
- *     disclaimer in the documentation and/or other materials provided
- *     with the distribution.
- *
- *     * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
- * GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
- * HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
- * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
- * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
- * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
- * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+
 #define ATRACE_TAG (ATRACE_TAG_GRAPHICS | ATRACE_TAG_HAL)
 #define DEBUG 0
 #include "QtiMapperExtensions.h"
@@ -453,59 +421,6 @@ Return<Error> QtiMapperExtensions::getSurfaceMetadata_V1(void *buffer, void *met
     }
   } else {
     ALOGE("%s: buffer pointer: %p, metadata pointer: %p ", __FUNCTION__, buffer, metadata);
-  }
-  return err;
-}
-
-Return<void> QtiMapperExtensions::getMetadataFd(void *buffer, getFd_cb hidl_cb) {
-  auto err = Error::BAD_BUFFER;
-  int fd = 0;
-  auto hnd = static_cast<private_handle_t *>(buffer);
-  if (buffer != nullptr && private_handle_t::validate(hnd) == 0) {
-    err = Error::NONE;
-    fd = hnd->fd_metadata;
-  } else {
-    ALOGE("%s: buffer pointer: %p ", __FUNCTION__, buffer);
-  }
-  hidl_cb(err, fd);
-  return Void();
-}
-
-Return<void> QtiMapperExtensions::getMetadataSize(void *buffer, getSize_cb hidl_cb) {
-  auto err = Error::BAD_BUFFER;
-  unsigned int size = 0;
-  auto hnd = static_cast<private_handle_t *>(buffer);
-  if (buffer != nullptr && private_handle_t::validate(hnd) == 0) {
-    err = Error::NONE;
-    size = sizeof(MetaData_t);
-  }
-  hidl_cb(err, size);
-  return Void();
-}
-
-Return<void> QtiMapperExtensions::getCRCBufferOffset(void *buffer, getOffset_cb hidl_cb) {
-  auto err = Error::BAD_BUFFER;
-  unsigned int offset = 0;
-  auto hnd = static_cast<private_handle_t *>(buffer);
-  if (buffer != nullptr && private_handle_t::validate(hnd) == 0) {
-    err = Error::NONE;
-    offset = offsetof(MetaData_t, crcBuffer);
-  }
-  hidl_cb(err, offset);
-  return Void();
-}
-
-Return<Error> QtiMapperExtensions::getCRCBufferData(void *buffer, void *data) {
-  auto err = Error::BAD_BUFFER;
-  auto hnd = static_cast<private_handle_t *>(buffer);
-  if (data != nullptr && buffer != nullptr && private_handle_t::validate(hnd) == 0) {
-    if (getMetaData(hnd, GET_CRC_BUFFER, data) == 0) {
-      err = Error::NONE;
-    } else {
-      err = Error::UNSUPPORTED;
-    }
-  } else {
-    ALOGE("%s: buffer pointer: %p, data pointer: %p ", __FUNCTION__, buffer, data);
   }
   return err;
 }
