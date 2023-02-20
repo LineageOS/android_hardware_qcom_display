dd4417456b200408644124d14e5e3c0cd1e054d2
Luofan Chen <clfbbn@gmail.com>
2023-02-20 14:35:13 +0800

    [SQUASH] Merge tag 'LA.UM.9.14.r1-20000.01-LAHAINA.QSSI13.0' into lineage-20.0-caf-sm8350
    "LA.UM.9.14.r1-20000.01-LAHAINA.QSSI13.0"
    
    Change-Id: I83b72cbb5161b4b5d5575a2e08c13fdf45516ac2

--- sdm/libs/core/drm/hw_device_drm.cpp
+++ sdm/libs/core/drm/hw_device_drm.cpp
@@ -27,6 +27,43 @@
 * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
+/*
+* Changes from Qualcomm Innovation Center are provided under the following license:
+*
+* Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted (subject to the limitations in the
+* disclaimer below) provided that the following conditions are met:
+*
+*    * Redistributions of source code must retain the above copyright
+*      notice, this list of conditions and the following disclaimer.
+*
+*    * Redistributions in binary form must reproduce the above
+*      copyright notice, this list of conditions and the following
+*      disclaimer in the documentation and/or other materials provided
+*      with the distribution.
+*
+*    * Neither the name of Qualcomm Innovation Center, Inc. nor the
+*      names of its contributors may be used to endorse or promote
+*      products derived from this software without specific prior
+*      written permission.
+*
+* NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+* GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
+* HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+* WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+* MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+* IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+* ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+* GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+* INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+* IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+* OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+* IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 #define __STDC_FORMAT_MACROS
 
 #include <ctype.h>
@@ -945,6 +982,10 @@ void HWDeviceDRM::SetDisplaySwitchMode(uint32_t index) {
       cmd_mode_index_ = current_mode_index_;
     }
   }
+  if (current_mode.hdisplay == to_set.hdisplay &&
+    current_mode.vdisplay == to_set.vdisplay) {
+    seamless_mode_switch_ = true;
+  }
 }
 
 DisplayError HWDeviceDRM::SetDisplayAttributes(uint32_t index) {
@@ -1041,14 +1082,18 @@ DisplayError HWDeviceDRM::PowerOff(bool teardown) {
   ResetROI();
   int64_t retire_fence_fd = -1;
   drmModeModeInfo current_mode = connector_info_.modes[current_mode_index_].mode;
-  drm_atomic_intf_->Perform(DRMOps::CRTC_SET_MODE, token_.crtc_id, &current_mode);
+  if (!IsSeamlessTransition()) {
+    drm_atomic_intf_->Perform(DRMOps::CRTC_SET_MODE, token_.crtc_id, &current_mode);
+  }
   drm_atomic_intf_->Perform(DRMOps::CONNECTOR_SET_POWER_MODE, token_.conn_id, DRMPowerMode::OFF);
   drm_atomic_intf_->Perform(DRMOps::CRTC_SET_ACTIVE, token_.crtc_id, 0);
   drm_atomic_intf_->Perform(DRMOps::CONNECTOR_GET_RETIRE_FENCE, token_.conn_id, &retire_fence_fd);
 
   int ret = NullCommit(false /* synchronous */, false /* retain_planes */);
   if (ret) {
-    DLOGE("Failed with error: %d", ret);
+    DLOGE("Failed with error: %d, dynamic_fps=%d, seamless_mode_switch_=%d, vrefresh_=%d,"
+     "panel_mode_changed_=%d bit_clk_rate_=%" PRIu64 , ret, hw_panel_info_.dynamic_fps,
+     seamless_mode_switch_, vrefresh_, panel_mode_changed_, bit_clk_rate_);
     return kErrorHardware;
   }
 
@@ -1270,8 +1315,15 @@ void HWDeviceDRM::SetupAtomic(Fence::ScopedRef &scoped_ref, HWLayers *hw_layers,
           drm_atomic_intf_->Perform(DRMOps::PLANE_SET_ZORDER, pipe_id, pipe_info->z_order);
 #endif
 
-          DRMBlendType blending = {};
-          SetBlending(layer.blending, &blending);
+          // Account for PMA block activation directly at translation time to preserve layer
+          // blending definition and avoid issues when a layer structure is reused.
+          DRMBlendType blending = DRMBlendType::UNDEFINED;
+          LayerBlending layer_blend = layer.blending;
+          if (layer_blend ==  kBlendingPremultiplied && pipe_info->inverse_pma_info.inverse_pma) {
+            layer_blend = kBlendingCoverage;
+            DLOGI_IF(kTagDriverConfig, "PMA handled by Inverse PMA block - Pipe id: %u", pipe_id);
+          }
+          SetBlending(layer_blend, &blending);
           drm_atomic_intf_->Perform(DRMOps::PLANE_SET_BLEND_TYPE, pipe_id, blending);
 
           DRMRect src = {};
@@ -1504,6 +1556,7 @@ DisplayError HWDeviceDRM::Validate(HWLayers *hw_layers) {
     DumpHWLayers(hw_layers);
     vrefresh_ = 0;
     panel_mode_changed_ = 0;
+    seamless_mode_switch_ = false;
     err = kErrorHardware;
   }
 
@@ -1604,6 +1657,7 @@ DisplayError HWDeviceDRM::AtomicCommit(HWLayers *hw_layers) {
     DumpHWLayers(hw_layers);
     vrefresh_ = 0;
     panel_mode_changed_ = 0;
+    seamless_mode_switch_ = false;
     return kErrorHardware;
   }
 
@@ -1674,6 +1728,7 @@ DisplayError HWDeviceDRM::AtomicCommit(HWLayers *hw_layers) {
   pending_power_state_ = kPowerStateNone;
   // Inherently a real commit ensures null commit properties have happened, so update the member
   first_null_cycle_ = false;
+  seamless_mode_switch_ = false;
 
   SetTUIState();
 
