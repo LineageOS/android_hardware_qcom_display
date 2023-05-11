53ecfe6584c50268fe4bf782d6f5104fa915e396
Saurabh Yadav <quic_sauyad@quicinc.com>
2023-05-11 04:51:46 -0700

    display: add stc config file for low ram targets
    This change adds new stc config file
    that loads default color library on low ram targets.
    
    Change-Id: I2985491284790856a3bf2ec395ffd047a8efcb0f
    CRs-Fixed: 3490302

--- config/display-product.mk
+++ config/display-product.mk
@@ -36,6 +36,9 @@ PRODUCT_PACKAGES += \
 ifneq ($(TARGET_HAS_LOW_RAM),true)
 #Multi-stc libraries config xml file
 PRODUCT_COPY_FILES += hardware/qcom/display/config/snapdragon_color_libs_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/snapdragon_color_libs_config.xml
+else
+#Default stc library config xml file
+PRODUCT_COPY_FILES += hardware/qcom/display/config/snapdragon_default_color_lib_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/snapdragon_color_libs_config.xml
 endif
 
 #QDCM calibration json file for r66451 panel
