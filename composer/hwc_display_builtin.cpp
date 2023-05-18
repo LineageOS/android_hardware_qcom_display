5bae5bdea1d5dae98d872e6bbc1eb32326cedbb1
danielml <daniel@danielml.dev>
2023-05-18 19:04:10 +0200

    sdm: hwc: Allow enabling doze mode support with a property
    * By default the HWC restricts the doze mode support
      to the dsi_cmd_mode panel type
    
    * This change allows enabling doze for devices that, with
      other panel types, still support it
    
    Change-Id: I50226dbbdd9dcdd947c7a13070eecdd2ec104aaf
    Signed-off-by: danielml <daniel@danielml.dev>

--- composer/hwc_display_builtin.cpp
+++ composer/hwc_display_builtin.cpp
@@ -201,6 +201,11 @@ int HWCDisplayBuiltIn::Init() {
   DebugHandler::Get()->GetProperty(ENABLE_ROUNDED_CORNER, &value);
   enable_round_corner_ = (value == 1);
 
+  value = 0;
+  DebugHandler::Get()->GetProperty(OVERRIDE_DOZE_MODE_PROP, &value);
+  override_doze_mode_ = (value == 1);
+  DLOGI("override_doze_mode: %d", override_doze_mode_);
+
   uint32_t config_index = 0;
   GetActiveDisplayConfig(&config_index);
   DisplayConfigVariableInfo attr = {};
