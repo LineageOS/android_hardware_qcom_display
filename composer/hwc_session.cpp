3fb9a47438226ff9fffb7c4562a49dd08a0d2695
kmiit <2421768138@qq.com>
2024-04-15 12:58:09 +0800

    display: Drop unused variable
    Change-Id: Ief2ba6372c93746993861d02c1ca0e4f4a8cd0cb

--- composer/hwc_session.cpp
+++ composer/hwc_session.cpp
@@ -3108,8 +3108,6 @@ int HWCSession::HandleConnectedDisplays(HWDisplaysInfo *hw_displays_info, bool d
   int status = 0;
   Display client_id = 0;
 
-  Display active_builtin = GetActiveBuiltinDisplay();
-
   for (auto &iter : *hw_displays_info) {
     auto &info = iter.second;
 
