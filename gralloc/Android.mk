74f1601a18e871db3f7350f0ed63282d516066f3
Ivan Vecera <ivan@cera.cz>
2024-07-03 11:40:30 +0200

    gralloc: Allow devices to opt-in for YCrCb camera video encode
    DeviceAsWebcam is using USAGE_VIDEO_ENCODE [1] for buffers and
    this results in combined usage: (VIDEO_ENCODER | CAMERA_OUTPUT).
    
    On some Qcom devices (verified on pyxis (sdm710)) and maybe others
    DeviceAsWebcam has red & blue colors swapped with HAL_PIXEL_FORMAT_YCbCr_420_SP_VENUS.
    
    When using HAL_PIXEL_FORMAT_YCrCb_420_SP_VENUS the output from
    DeviceAsWebcam has correct colors.
    
    [1] https://android.googlesource.com/platform/packages/services/DeviceAsWebcam/+/d079a82ba4f773210c01d488d2dfa1d4d25be8e2
    
    Change-Id: I4376102e2b0eeaefed536eff29c315a71e469b57

--- gralloc/Android.mk
+++ gralloc/Android.mk
@@ -60,6 +60,9 @@ endif
 ifeq ($(TARGET_NO_RAW10_CUSTOM_FORMAT),true)
     LOCAL_CFLAGS              += -DNO_RAW10_CUSTOM_FORMAT
 endif
+ifeq ($(TARGET_USES_YCRCB_CAMERA_ENCODE),true)
+    LOCAL_CFLAGS              += -DUSE_YCRCB_CAMERA_ENCODE
+endif
 
 LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps)
 LOCAL_SRC_FILES               := gr_utils.cpp gr_adreno_info.cpp gr_camera_info.cpp
--- gralloc/gr_utils.cpp
+++ gralloc/gr_utils.cpp
@@ -1425,7 +1425,15 @@ int GetImplDefinedFormat(uint64_t usage, int format) {
       } else if (usage & GRALLOC_USAGE_PRIVATE_HEIF) {
         gr_format = HAL_PIXEL_FORMAT_NV12_HEIF;
       } else if (format == HAL_PIXEL_FORMAT_YCbCr_420_888) {
+#ifdef USE_YCRCB_CAMERA_ENCODE
+        if (usage & BufferUsage::CAMERA_OUTPUT) {
+          gr_format = HAL_PIXEL_FORMAT_YCrCb_420_SP_VENUS;
+        } else {
+          gr_format = HAL_PIXEL_FORMAT_YCbCr_420_SP_VENUS;
+        }
+#else
         gr_format = HAL_PIXEL_FORMAT_YCbCr_420_SP_VENUS;
+#endif
       } else {
         gr_format = HAL_PIXEL_FORMAT_NV12_ENCODEABLE;  // NV12
       }
