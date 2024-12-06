3cba7b0583f6382c71af5300f4c5f6edf7a3f53b
Atakan <ata.kaner03@gmail.com>
2024-12-06 12:30:45 +0300

    sdm: Support Samsung FINGERPRINT_MASK DRM property
    Allow Samsung devices to enable their HBM mode when a dim layer is present.
    
    Change-Id: I200d06d1e17dbad55715d4fc8e1ce812ff0f0ec9

--- libdrmutils/drm_interface.h
+++ libdrmutils/drm_interface.h
@@ -505,6 +505,14 @@ enum struct DRMOps {
    * Arg: drmModeAtomicReq - Atomic request
    */
   RESET_PANEL_FEATURES,
+#ifdef CONNECTOR_PROP_UDFPS
+  /*
+   * Op: Sets fingerprint mask on this connector
+   * Arg: uint32_t - Connector ID
+   *      uint32_t - fingerprint mask
+   */
+  CONNECTOR_SET_FINGERPRINT_MASK,
+#endif
 };
 
 enum struct DRMRotation {
--- sde-drm/drm_property.cpp
+++ sde-drm/drm_property.cpp
@@ -180,6 +180,9 @@ DRMProperty DRMPropertyManager::GetPropertyEnum(const std::string &name) const {
   if (name == "dimming_bl_lut") { return DRMProperty::DIMMING_BL_LUT; }
   if (name == "dimming_dyn_ctrl") { return DRMProperty::DIMMING_DYN_CTRL; }
   if (name == "dimming_min_bl") { return DRMProperty::DIMMING_MIN_BL; }
+#ifdef CONNECTOR_PROP_UDFPS
+  if (name == "fingerprint_mask") { return DRMProperty::FINGERPRINT_MASK; }
+#endif
 
   return DRMProperty::INVALID;
 }
