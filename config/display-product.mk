b90de0f005f2058e49de43e1375865d21e58b5f4
ata-kaner <ata.kaner03@gmail.com>
2025-01-22 17:45:44 -0800

    sdm: Support FINGERPRINT_MASK property for Samsung
    In OneUI, Samsung controls the HBM with its custom logic from its oldest devices up to the SM8550.
    
    In order to improve Optical Samsung devices, provide the same logic here. This will eliminate the need of complex patches to the techpack between different SoCs
    while providing a much better HBM performance and dimming solution.
    
    Change-Id: I200d06d1e17dbad55715d4fc8e1ce812ff0f0ec9

--- config/display-product.mk
+++ config/display-product.mk
@@ -156,7 +156,7 @@ endif
 SOONG_CONFIG_NAMESPACES += qtidisplay
 
 # Soong Keys
-SOONG_CONFIG_qtidisplay := drmpp headless llvmsa gralloc4 displayconfig_enabled udfps default var1 var2 var3 llvmcov
+SOONG_CONFIG_qtidisplay := drmpp headless llvmsa gralloc4 displayconfig_enabled udfps samsung_fingerprint default var1 var2 var3 llvmcov
 
 # Soong Values
 SOONG_CONFIG_qtidisplay_drmpp := true
@@ -165,6 +165,7 @@ SOONG_CONFIG_qtidisplay_llvmsa := false
 SOONG_CONFIG_qtidisplay_gralloc4 := true
 SOONG_CONFIG_qtidisplay_displayconfig_enabled := false
 SOONG_CONFIG_qtidisplay_udfps := false
+SOONG_CONFIG_qtidisplay_samsung_fingerprint := false
 SOONG_CONFIG_qtidisplay_default := true
 SOONG_CONFIG_qtidisplay_var1 := false
 SOONG_CONFIG_qtidisplay_var2 := false
