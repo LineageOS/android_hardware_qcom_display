8813dfe5bf8f3449b0571be45dce6eb20e04ead7
Alexander Koskovich <akoskovich@pm.me>
2025-05-21 18:08:50 -0400

    composer: Fix compile
    hardware/qcom-caf/sm8650/display/composer/hwc_session.cpp:188:22: error: use of undeclared identifier 'PAGE_SIZE'
      188 |     char uevent_data[PAGE_SIZE] = {};
          |                      ^
    1 error generated.
    
    Change-Id: Iac3d348dcdca8c5e67201ef7bc7a0552c5b1a9c6

--- composer/hwc_session.cpp
+++ composer/hwc_session.cpp
@@ -185,7 +185,7 @@ void HWCSession::HpdThreadTop() {
   }
 
   while (1) {
-    char uevent_data[PAGE_SIZE] = {};
+    char uevent_data[kPageSize] = {};
 
     // keep last 2 zeros to ensure double 0 termination
     int length = uevent_next_event(uevent_data, INT32(sizeof(uevent_data)) - 2);
