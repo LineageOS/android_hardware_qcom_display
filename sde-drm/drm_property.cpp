0dad38d9acb6b82a33d647f4fa93c31e98d31dab
pjgowtham <pjgowtham@gmail.com>
2025-05-22 12:46:34 +0530

    sdm: Operate FINGERPRINT_MASK for oplus optical UDFPS
    Even though oplus calls the connector property hbm_enable, its function
    is not just limited to operating HBM. It is also responsible for other
    aspects of UDFPS operations like that of setFpPress so that we don't
    have to handle any of these through IOCTL.
    
    The purpose of this DRM connector property is for the userspace to let
    the kernel display and fingerprint drivers know which layers are
    visible. The same can be achieved by asserting with the layer name
    instead of setting a magic constant for the layer ZOrder, thereby
    eliminating the need for SurfaceFlinger hacks that we previously needed
    with prebuilt HWC.
    
    | Bit index | Decimal | Layer       | Z-order      |
    | --------- | ------- | ----------- | ------------ |
    | 0         | 1       | **dim**     | `0x41000005` |
    | 1         | 2       | **pressed** | `0x41000033` |
    | 2         | 4       | **icon**    | `0x41000032` |
    | 3         | 8       | **aod**     | `0x41000011` |
    
    Change-Id: I9fcb119bbebf44b2cd13fb465c09cd9d332013ed

--- sde-drm/drm_property.cpp
+++ sde-drm/drm_property.cpp
@@ -227,7 +227,7 @@ DRMProperty DRMPropertyManager::GetPropertyEnum(const std::string &name) const {
   if (name == "SDE_SSPP_FP16_CSC_V1") { return DRMProperty::SDE_SSPP_FP16_CSC_V1; }
   if (name == "SDE_SSPP_FP16_UNMULT_V1") { return DRMProperty::SDE_SSPP_FP16_UNMULT_V1; }
 #ifdef CONNECTOR_PROP_UDFPS
-  if (name == "fingerprint_mask") { return DRMProperty::FINGERPRINT_MASK; }
+  if (name == "fingerprint_mask" || name == "hbm_enable") { return DRMProperty::FINGERPRINT_MASK; }
 #endif
 
   return DRMProperty::INVALID;
