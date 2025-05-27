74b1c158fb66cdfa6c263b50ecac956b1855d860
Alexander Koskovich <akoskovich@pm.me>
2025-05-27 12:24:58 -0400

    composer: Use getpagesize() instead of PAGE_SIZE
    Required for targets that have VSR_VENDOR_API_LEVEL >= 35.
    
    display/composer/hwc_session.cpp:188:22: error: use of undeclared identifier 'PAGE_SIZE'
      188 |     char uevent_data[PAGE_SIZE] = {};
          |                      ^
    1 error generated.
    
    Change-Id: I3a0c2fe5ead48526e2e6b2bab490d8e24e9e0f65

--- composer/hwc_session.cpp
+++ composer/hwc_session.cpp
@@ -184,13 +184,14 @@ void HWCSession::HpdThreadTop() {
     return;
   }
 
+  auto page_size = static_cast<unsigned int>(getpagesize());
+  std::vector<char> uevent_data(page_size, 0);
+
   while (1) {
-    char uevent_data[PAGE_SIZE] = {};
-
     // keep last 2 zeros to ensure double 0 termination
-    int length = uevent_next_event(uevent_data, INT32(sizeof(uevent_data)) - 2);
+    int length = uevent_next_event(uevent_data.data(), INT32(page_size) - 2);
 
-    ParseUEvent(uevent_data, length);
+    ParseUEvent(uevent_data.data(), length);
   }
   DLOGI("Ending!");
 }
