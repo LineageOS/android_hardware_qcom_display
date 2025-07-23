d3b45f190d492f93b4c1d12f1073695aaea1173e
Arian <arian.kulmer@web.de>
2025-07-23 15:04:35 +0200

    composer: Get rid of PAGE_SIZE usage
    Change-Id: Ie40ae4e00efe68551565c9fc32e5358494ddbe00

--- composer/hwc_session.cpp
+++ composer/hwc_session.cpp
@@ -185,12 +185,12 @@ void HWCSession::HpdThreadTop() {
   }
 
   while (1) {
-    char uevent_data[PAGE_SIZE] = {};
+    std::vector<char> uevent_data(getpagesize());
 
     // keep last 2 zeros to ensure double 0 termination
-    int length = uevent_next_event(uevent_data, INT32(sizeof(uevent_data)) - 2);
+    int length = uevent_next_event(uevent_data.data(), INT32(uevent_data.size() * sizeof(char)) - 2);
 
-    ParseUEvent(uevent_data, length);
+    ParseUEvent(uevent_data.data(), length);
   }
   DLOGI("Ending!");
 }
