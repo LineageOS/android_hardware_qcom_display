2f074f55c6f37afecd2b0d163b6ccd0755c7bd70
John Galt <johngaltfirstrun@gmail.com>
2026-01-14 08:14:01 -0500

    composer: Support Pixelworks HW iris 7 color calibration
    Change-Id: I8e9c72e608d74c5eb61764291a8e9a4ce11519b2

--- include/pxlw_iris_wrapper.h
+++ include/pxlw_iris_wrapper.h
@@ -5,6 +5,8 @@
 
 #pragma once
 
+#include <cstdint>
+
 namespace android {
 namespace hardware {
 namespace graphics {
@@ -28,6 +30,18 @@ class PxlwIrisWrapper {
 
  private:
   PxlwIrisWrapper() = default;
+};
+
+// HW Iris 7 (AIDL-based) wrapper entry points exported by vendor libpwirishalwrapper.so.
+// Only used when SUPPORTS_PXLW_IRIS7 is defined.
+class PxlwIris7AidlWrapper {
+ public:
+  int InitPrimaryDisplay(int param_1, unsigned int param_2, unsigned int param_3);
+  void SetColorModeWithRenderIntent(int param_1, int param_2, int32_t param_3, int32_t param_4);
+
+ private:
+  PxlwIris7AidlWrapper() = default;
+  friend class PxlwIrisWrapper;
 };
 
 class PxlwSoftirisWrapper {
